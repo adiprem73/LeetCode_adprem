7868bdb48a9d3e599d1a083a2dd6d1db951a38bf
Aditya Prem <aditya.prem2003@gmail.com>
2025-03-16 17:27:20 +0530

    Time: 0 ms (100%), Space: 19.2 MB (1.74%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    #define ll long long
+#define push(a) push_back(a)
+#define f(i, a, b) for(int i=a;i<b;i++)
+#define all(x) x.begin(),x.end()
+#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
+#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
+#define vint vector<int>
+#define vstring vector<string>
+#define vmat vector<vector<int>>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define mii map<int,int>
+#define mll map<ll,ll>
+
+const int INF = 1e9;
+const ll MOD = 1e9+7;
+
+bool search(vector<int>& nums, int target) {
+    mii mp;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        mp[nums[i]]=i;
+    }
+    sort(nums.begin(),nums.end());
+    int s=0;
+    int e=n-1;
+    int mid;
+    int res=-1;
+    while(s<=e){
+        mid=s+(e-s)/2;
+
+        if(nums[mid]==target){
+            res=mid;
+        }
+        if(nums[mid]<target){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    // cout<<"Res : "<<res<<endl;
+    if(res!=-1){
+        return true;
+    }
+    return false;
+}
+};
