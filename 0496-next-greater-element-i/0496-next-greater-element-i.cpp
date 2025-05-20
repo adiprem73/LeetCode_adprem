f0ec8d3311ab1a6a9f9d40275605f9855764fd1e
Aditya Prem <aditya.prem2003@gmail.com>
2025-05-20 11:22:11 +0530

    Time: 3 ms (43.03%), Space: 12.9 MB (13.98%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -18,24 +18,45 @@ public:
 const int INF = 1e9;
 const ll MOD = 1e9+7;
 
-int nextGreatElement(vint nums, int n){
-    bool flag=false;
-    int ans=-1;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==n){
-            flag=true;
+// int nextGreatElement(vint nums, int n){
+//     bool flag=false;
+//     int ans=-1;
+//     for(int i=0;i<nums.size();i++){
+//         if(nums[i]==n){
+//             flag=true;
+//         }
+//         if(flag==true&&nums[i]>n){
+//             return nums[i];
+//         }
+//     }
+//     return -1;
+// }
+
+map<int,int> nge(vector<int> &nums2){
+    stack<int> st;
+    vint ans(nums2.size());
+    for(int i=nums2.size()-1;i>=0;i--){
+        while(!st.empty()&&st.top()<=nums2[i]){
+            st.pop();
         }
-        if(flag==true&&nums[i]>n){
-            return nums[i];
-        }
+        if(st.empty())ans[i]=-1;
+        else ans[i]=st.top();
+        st.push(nums2[i]);
     }
-    return -1;
+    // vprint(ans);
+    map<int,int> result;
+    for(int i=0;i<nums2.size();i++){
+        result[nums2[i]]=ans[i];
+    }
+    // mprint(result);
+    return result;
 }
 
 vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2){
     vint ans;
+    map<int,int> res=nge(nums2);
     for(int i=0;i<nums1.size();i++){
-        ans.push_back(nextGreatElement(nums2,nums1[i]));
+        ans.push_back(res[nums1[i]]);
     }
     return ans;
 }
