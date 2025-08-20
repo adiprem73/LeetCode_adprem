e239d963bf26917d8d379d333f88a44291ff6053
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-20 17:59:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>> & dp){
-    if(i==0 && j==0){
-        return 1;
-    }
-
-    if(i<0 || j<0){
-        return 0;
-    }
-
-    if(dp[i][j]!= -1)return dp[i][j];
-
-    int left = func(i, j-1, dp);
-    int right= func(i-1, j, dp);
-
-    return dp[i][j]= left + right;
-}
-
-int uniquePaths(int m, int n){
+    int uniquePaths(int m, int n){
     vector<vector<int>> dp(m, vector<int>(n, -1));
-    return func(m-1, n-1, dp);
+    dp[0][0]=1; //base case: only one wya to reach the start from (0,0)
+
+    for(int i=0; i<m;i++){
+        for(int j=0;j<n;j++){
+            if(!(i==0&&j==0)){ //excludig the base case from the loops
+
+                int right=0;
+                int down=0;
+                if(j>0){
+                    right= dp[i][j-1];
+                }
+
+                if(i>0){
+                    down= dp[i-1][j];
+                }
+                dp[i][j] = down+right;
+            }
+        }
+    }
+
+    
+
+    return dp[m-1][n-1];
 }
 };
