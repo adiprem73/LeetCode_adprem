88faa2381ca23e3a841ad36750c21f37155bbf02
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-24 18:49:39 +0530

    Time: 15 ms (75.33%), Space: 28.6 MB (39.83%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int func(int ind, int amount, vector<int> &coins, vector<vector<int>>& dp)
+{
+    // base case
+    if (amount == 0)
+        return 1;
+    if (ind < 0)
+        return 0;
+    if (amount < 0)
+        return 0;
+
+    if(dp[ind][amount]!=-1)return dp[ind][amount];
+    int notPick = func(ind - 1, amount, coins, dp);
+    int pick = 0;
+    if (coins[ind] <= amount)
+    {
+        pick = func(ind, amount - coins[ind], coins, dp);
+    }
+    return dp[ind][amount]=pick + notPick;
+}
+
+int change(int amount, vector<int> &coins)
+{
+    int n = coins.size();
+    vector<vector<int>> dp(n, vector<int> (amount+1, -1));
+    return func(n - 1, amount, coins, dp);
+}
+};
