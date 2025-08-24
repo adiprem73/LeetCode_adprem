7d2db1491c6b30e9c6f45ffc11ebe95e7c6208fe
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-24 18:28:27 +0530

    Time: 23 ms (44.91%), Space: 25.1 MB (33.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-int func(int ind, int target, vector<int> & nums, vector<vector<int>> &dp, int totalSum){
-    if(ind ==0){
-        if(target==0 && nums[0]==0) return 2;
-
-        if(target==nums[0] || target== -nums[0]) return 1;
-        return 0;
-    }
-
-    if(abs(target) > totalSum) return 0;
-
-
-    if(dp[ind][target +totalSum]!= -1)return dp[ind][target+totalSum];
-
-    int plus=func(ind-1, target- nums[ind], nums, dp, totalSum);
-
-    int minus=func(ind-1, target+ nums[ind], nums, dp, totalSum);
-    return dp[ind][target+totalSum]=plus+minus;
-}
-
 int findTargetSumWays(vector<int>& nums, int target) {
     int n=nums.size();
     int totalSum=accumulate(nums.begin(), nums.end(),0);
 
+    if(abs(target)>totalSum) return 0;
     vector<vector<int>> dp(n, vector<int>(2* totalSum + 1, -1));
-    return func(n-1, target, nums, dp, totalSum);
+
+    for(int j=-totalSum ; j<=totalSum ; j++){
+        if (nums[0]==0 && j==0) dp[0][j+totalSum]=2;
+        else if (j==nums[0] || j==-nums[0]) dp[0][j+totalSum]=1;
+        else dp[0][j+ totalSum] = 0;
+    }
+    
+
+    for(int i=1;i<n;i++){
+        for(int j=-totalSum;j<=totalSum;j++){
+            int plus=(j-nums[i] >= -totalSum)? dp[i-1][j- nums[i]+totalSum] : 0;
+
+            int minus=(j+nums[i] <= totalSum)? dp[i-1][j+ nums[i]+totalSum] : 0;
+
+            dp[i][j+totalSum]=plus + minus;
+        }
+    }
+
+    return dp[n-1][target+totalSum];
+
 }
 };
