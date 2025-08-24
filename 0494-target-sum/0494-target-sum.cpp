2529054057523c0c271ec3c88bd117a19a2b8918
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-24 11:56:13 +0530

    Time: 8 ms (62.78%), Space: 25.9 MB (25.83%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-int func(int ind, int target, vector<int> & nums){
+int func(int ind, int target, vector<int> & nums, vector<vector<int>> &dp, int totalSum){
     if(ind ==0){
         if(target==0 && nums[0]==0) return 2;
 
         if(target==nums[0] || target== -nums[0]) return 1;
+        return 0;
     }
 
-    if(ind<0)return 0;
+    if(abs(target) > totalSum) return 0;
 
-    int plus=func(ind-1, target- nums[ind], nums);
 
-    int minus=func(ind-1, target+ nums[ind], nums);
+    if(dp[ind][target +totalSum]!= -1)return dp[ind][target+totalSum];
 
-    return plus+minus;
+    int plus=func(ind-1, target- nums[ind], nums, dp, totalSum);
+
+    int minus=func(ind-1, target+ nums[ind], nums, dp, totalSum);
+    return dp[ind][target+totalSum]=plus+minus;
 }
 
 int findTargetSumWays(vector<int>& nums, int target) {
     int n=nums.size();
-    return func(n-1, target, nums);
+    int totalSum=accumulate(nums.begin(), nums.end(),0);
+
+    vector<vector<int>> dp(n, vector<int>(2* totalSum + 1, -1));
+    return func(n-1, target, nums, dp, totalSum);
 }
 };
