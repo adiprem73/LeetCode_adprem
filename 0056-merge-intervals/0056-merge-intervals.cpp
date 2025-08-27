3f2199e69ed3e1f55431fba13883b2820578bf89
Aditya Prem <aditya.prem2003@gmail.com>
2025-08-28 00:57:28 +0530

    Time: 9 ms (19.06%), Space: 24.9 MB (11.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-vector<vector<int>> merge(vector<vector<int>>& intervals) {
-    sort(intervals.begin(), intervals.end());
-    for(int i=0;i<intervals.size()-1;i++){
-        if(intervals[i][1]>=intervals[i+1][0]){
-            intervals[i][1]= max(intervals[i][1], intervals[i+1][1]);
-            intervals.erase(intervals.begin()+i+1);
-            i--;
+    vector<vector<int>> merge(vector<vector<int>> & intervals){
+    int n=intervals.size();
+    vector<vector<int>> mergedIntervals;
+    if(n==0){
+        return mergedIntervals;
+    }
+    vector<int> tempInterval;
+        sort(intervals.begin(), intervals.end());
+
+    tempInterval = intervals[0];
+
+    for(auto it: intervals){
+        if(it[0]<=tempInterval[1]){
+            tempInterval[1]= max(it[1], tempInterval[1]);
         }
-    } 
-    return intervals;
+        else{
+            mergedIntervals.push_back(tempInterval);
+            tempInterval=it;
+        }
+    }
+    mergedIntervals.push_back(tempInterval);
+    return mergedIntervals;
 }
 };
