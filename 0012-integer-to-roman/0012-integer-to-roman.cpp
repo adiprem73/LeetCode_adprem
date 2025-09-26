e2b17f54ebb1b39f66bae6409d14e3b3becbc09f
Aditya Prem <aditya.prem2003@gmail.com>
2025-09-27 02:39:09 +0530

    Time: 18 ms (5.26%), Space: 16.1 MB (5.19%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string intToRoman(int num)
+{
+    map<int, string, greater<int>> mp = {
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"},
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    };
+    string ans;
+    // now we will apply greedy
+    for(auto it: mp){
+        while(num>=it.first){
+            num-=it.first;
+            ans+=it.second;
+        }
+        // cout<<num<<endl;
+        // cout<<ans<<endl;
+        if(num==0){
+            break;
+        }
+    }
+    return ans;
+}
+};
