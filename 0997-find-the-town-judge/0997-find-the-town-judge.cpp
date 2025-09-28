89f7da4e211a6a235e4bee0dead608908546e58b
Aditya Prem <aditya.prem2003@gmail.com>
2025-09-29 00:22:08 +0530

    Time: 37 ms (18.83%), Space: 73.1 MB (38.77%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,42 +2,13 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>> &trust)
 {
-    if(trust.size()==0){
-        if(n>1)return -1;
-        else return n;
-    }
-    if(trust.size()==1 && trust[0].size()==0){
-        if(n>1)return -1;
-        else return n;
-    }
-    vector<vector<int>> gp(n+1);
+    vector<int> score(n+1);
     for(auto it: trust){
-        gp[it[0]].push_back(it[1]);
+        score[it[1]]++;
+        score[it[0]]--;
     }
-    // graph is ready
-    vector<int> judgeCandidates; //ones who dont have anybody who they trust
-    for(int i=1;i<=n;i++){
-        if(gp[i].size()==0){
-            judgeCandidates.push_back(i);
-        }
-    }
-    // now we just need to search others. search for whom does everyone trust
-    for(int it: judgeCandidates){
-        bool flag=true;
-        for (int i = 1; i <= n; i++)
-        {
-            if (i!=it)
-            {
-                if(find(gp[i].begin(), gp[i].end(), it)==gp[i].end()){
-                    flag=false;
-                    break;
-                }
-            }
-        }
-        if(flag==true){
-            return it;
-        }
+    for(int i=1;i<n+1;i++){
+        if(score[i]==n-1)return i;
     }return -1;
-    
 }
 };
