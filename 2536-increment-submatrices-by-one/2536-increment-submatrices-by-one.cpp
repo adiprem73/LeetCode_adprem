ffefb22652cd8ad2b69d3e1505957f574b53380f
Aditya Prem <aditya.prem2003@gmail.com>
2025-11-15 09:20:53 +0530

    Time: 43 ms (48.29%), Space: 98.3 MB (7.48%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+    vector<vector<int>> rangeAddQueries2(int n, vector<vector<int>>& queries) {
     vector<vector<int>> vec(n, vector<int>(n,0));
     for(auto it: queries){
         int r1= it[0];
@@ -19,5 +19,65 @@ public:
     //     vprint(it);
     // }
     return vec;
+}
+
+vector<vector<int>> rangeAddQueries(int n, vector<vector<int>> &queries)
+{
+    vector<vector<int>> diff(n+1, vector<int>(n+1, 0));
+    for (auto it : queries)
+    {
+        int r1 = it[0];
+        int c1 = it[1];
+        int r2 = it[2];
+        int c2 = it[3];
+        diff[r1][c1]+=1;
+        diff[r1][c2+1]-=1;
+        diff[r2+1][c1]-=1;
+        diff[r2+1][c2+1]+=1;
+
+        // now we need to use prefix sums to spread the additions of 1 across the table
+
     }
+
+    // row wise sum
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += diff[i][j];
+
+            diff[i][j] = sum;
+        }
+    }
+
+    // column wise sum
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += diff[j][i];
+
+            diff[j][i] = sum;
+        }
+    }
+
+    // for(auto it: diff){
+    //     vprint(it);
+    // }
+    vector<vector<int>> vec(n, vector<int> (n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            vec[i][j]= diff[i][j];
+        }
+    }
+    // cout<<"Hello"<<endl;
+    // for (auto it : vec)
+    // {
+    //     vprint(it);
+    // }
+    return vec;
+}
+
 };
