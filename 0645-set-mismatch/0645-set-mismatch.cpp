1661c0b6bf05119464bbddce0440113841153d03
Aditya Prem <aditya.prem2003@gmail.com>
2025-11-16 17:33:38 +0530

    Time: 2 ms (53.82%), Space: 26.2 MB (49.98%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
-        int n= nums.size();
-        map<int,int> mp;
-        unordered_set<int> st;
-        for(auto it: nums){
-            mp[it]++;
-            st.insert(it);
-        }
-        int extra, missing;
-        for(auto it: mp){
-            if(it.second==2){
-                extra = it.first;
-                break;
-            }
-        }
-        for(int i=1;i<=n;i++){
-            if(st.count(i)==0){
-                missing= i;
-                break;
-            }
-        }
-        return {extra, missing};
+    vector<int> findErrorNums(vector<int> &nums)
+{
+    int n= nums.size();
+    vector<int> mp(n+1, 0);
+
+    for(int i=0;i<n;i++){
+        mp[nums[i]]++;
     }
+
+    int missing, extra;
+    for(int i=0;i<mp.size();i++){
+        if(mp[i]==2){
+            extra= i;
+        }
+        if(mp[i]==0){
+            missing= i;
+        }
+    }
+    return {extra, missing};
+}
 };
