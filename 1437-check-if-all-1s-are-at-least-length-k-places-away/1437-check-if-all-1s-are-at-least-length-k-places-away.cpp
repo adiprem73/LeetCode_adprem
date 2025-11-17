9cc97c1a6fa31a47119f14e1b11ba73f30825785
Aditya Prem <aditya.prem2003@gmail.com>
2025-11-17 14:47:31 +0530

    Time: 3 ms (15.64%), Space: 61.4 MB (74.91%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -2,21 +2,11 @@ class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
         int n= nums.size();
-        for(int i=0;i<n;i++){
-            
-            if(nums[i]==1 && i!=n-1){
-                int steps=0;
-                i++;
-                while(i<n && nums[i]==0){
-                    steps++;
-                    i++;
-                }
-                if(i<n && nums[i]==1){
-                    if(steps<k)return false;
-                }
-                i--;
-                cout<<steps<<endl;
-                
+        int last=-1;
+        for(int i=0;i<n;++i){
+            if(nums[i]==1){
+                if(last!=-1 && i-last-1<k)return false;
+                last =i;
             }
         }
         return true;
