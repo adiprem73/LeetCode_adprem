32d6c097c6b3c64625b1fb5630a849a643b29eeb
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-15 19:11:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void dfs(int u, vector<vector<int>>& adj, vector<bool> & visited, int& count){
+    // first thing is to makr that node as visited
+    visited[u]= true;
+    count++;
+
+    for(auto neighbour : adj[u]){
+        if(visited[neighbour]== false){
+            dfs(neighbour, adj, visited, count);
+        }
+    }
+}
+
+// (x-a)2 + (y-b)2 = r2
+
+int maximumDetonation(vector<vector<int>>& bombs) {
+    int n = bombs.size();
+    vector<vector<int>> adj(n);
+
+    // make the adjacency vector based on what bombs will detoaonte what bombs
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j){
+                long long g = bombs[i][0] - bombs[j][0];
+                long long h = bombs[i][1] - bombs[j][1];
+                if (g*g + h*h <= bombs[i][2]*bombs[i][2])
+                {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+    } 
+    
+    // then we just need to find the length of the maximum spanning tree
+    int ans =0;
+    
+    for(int i=0;i<n;i++){
+        // we msut perform fresh detonations and fresh chain reactions for eahc node and then take the maximum one
+        int count = 0;
+        vector<bool> visited(n, false);
+        dfs(i, adj, visited, count);
+        ans = max(ans, count);
+    }
+    return ans;
+}
+};
