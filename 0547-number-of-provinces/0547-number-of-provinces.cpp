f8db8561eff12bb6900293aa82fcc2add36c8ba6
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-15 17:42:50 +0530

    Time: 10 ms (6.91%), Space: 20.8 MB (25.99%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>> & adj, vector<int>& vis){
-    vis[node]= 1;
-    for(auto it: adj[node]){
-        if(!vis[it]){
-            dfs(it, adj, vis);
+    void dfs(int u, vector<vector<int>> &adj, vector<bool> & visited){
+    // mark this visited
+    visited[u]= true;
+    for(auto neighbours: adj[u]){ // traverse through all the neighbouring nodes
+        if(visited[neighbours] == false){
+            dfs(neighbours, adj, visited);
         }
-    }    
+    }
 }
 
-int findCircleNum(vector<vector<int>>& isConnected) {
-    int n=isConnected.size();
-    // vector<int> adjLs[n];
-    vector<vector<int>> adjLs(n);
+int findCircleNum(vector<vector<int>> &isConnected)
+{
+    // the very first thing to do will be to convert the graph from matrix form to vector form so that we can perform  our traversals easily
+    int n = isConnected.size();
+    vector<vector<int>> adj(n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(isConnected[i][j]==1 && i!=j){
-                adjLs[i].push_back(j);
-                adjLs[j].push_back(i);
+            if(isConnected[i][j]==1){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
             }
         }
     }
-    // int vis[n]={0};
-    vector<int> vis(n);
+
+    // we will maintain a count variable to count the number of times dfs gets called from this main function: this will only give the correct number of provinces
     int cnt=0;
+
+
+    // now first we will try for a dfs approach
+    // for this we will need a visited array as well so that we do not go aorund in circles
+    vector<bool> visited(n);
     for(int i=0;i<n;i++){
-        if(!vis[i]){
+        if(visited[i]== false){
+            dfs(i, adj, visited);
             cnt++;
-            dfs(i, adjLs, vis);
         }
     }
     return cnt;
