c108f6bd1e1449c6cf87f4332e5180ac7af23b46
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-16 17:17:52 +0530

    Time: 0 ms (100%), Space: 18.5 MB (15.08%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-    int n=image.size();
-    int m=image[0].size();
-
-    queue<pair<int,int>> q;
-    vector<vector<int>> vis(n, vector<int>(m,0));
-
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+{
+    int m = image.size();
+    int n = image[0].size();
+    // let us do this quesitons by using BFS
+    queue<pair<int,int>>q;
     q.push({sr,sc});
-    int clr=image[sr][sc];
-    image[sr][sc]=color;
-    vis[sr][sc]=color;
-    
-    vector<int> drow={-1,0,1,0};
-    vector<int> dcol={0,1,0,-1};
-
+    vector<int> dx = {-1, 0, 1 , 0};
+    vector<int> dy = {0, 1, 0, -1};
+    vector<vector<bool>> visited(m, vector<bool>(n,false));
+    visited[sr][sc]= true;
     while(!q.empty()){
-        auto node=q.front();
-        int r=node.first;
-        int c=node.second;
+        // we mustmove in all four directions 
+        auto [x, y] = q.front();
         q.pop();
+        int prevcolor = image[x][y];
+        image[x][y]= color;
+        // we do not need to perform layer by layer bfs so we will jsut do normal bfs
         for(int i=0;i<4;i++){
-            int nrow=r+drow[i];
-            int ncol=c+dcol[i];
-            if(nrow>=0 && nrow<n && ncol<m && ncol>=0 && vis[nrow][ncol]!= color && image[nrow][ncol]==clr){
-                q.push({nrow,ncol});
-                image[nrow][ncol]=color;
-                vis[nrow][ncol]=color;
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && visited[nx][ny]==false && image[nx][ny] == prevcolor){
+                q.push({nx, ny});
+                visited[nx][ny] = true;
             }
         }
-        // cout<<"r : "<<r<<" c : "<<c<<endl;
-        // for(int i=0;i<image.size();i++){
-        //     vprint(image[i]);
-        // }
     }
     return image;
 }
