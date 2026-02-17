9da9999b6f65b046a3412a0d090f2adcd4b0e6be
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-18 01:22:25 +0530

    Time: 91 ms (5.02%), Space: 16.5 MB (11.41%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,53 +1,51 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<char>> &board, vector<vector<int>> &vis)
-{
-    vis[r][c] = 1;
-    int n = board.size();
-    int m = board[0].size();
-    vector<int> drow = {-1, 0, 1, 0};
-    vector<int> dcol = {0, 1, 0, -1};
-    for (int i = 0; i < 4; i++)
-    {
-        int nrow = r + drow[i];
-        int ncol = c + dcol[i];
-        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && board[nrow][ncol] == 'O' && vis[nrow][ncol] != 1)
-        {
-            dfs(nrow, ncol, board, vis);
+    void dfs(int x, int y, vector<vector<int>> &vis, vector<vector<char>>& board){
+    int m = board.size();
+    int n = board[0].size();
+    vis[x][y]=1; // makr them visited
+    vector<int> dx = {-1, 0, 1, 0};
+    vector<int> dy = {0,1,0,-1};
+    for(int i=0;i<4;i++){
+        int nx = x+dx[i];
+        int ny = y+dy[i];
+        if(nx>=0 && nx<m && ny>=0 && ny<n && vis[nx][ny]==0 && board[nx][ny]=='O'){
+            dfs(nx, ny, vis, board);
         }
     }
 }
 
-void solve(vector<vector<char>> &board)
-{
-    int n = board.size();
-    int m = board[0].size();
-    vector<vector<int>> vis(n, vector<int>(m, 0));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            // only run for boundary conditions
-            if (i == 0 || i == n - 1 || j == 0 || j == m - 1)
-            {
-                if (board[i][j] == 'O' && vis[i][j] != 1)
-                {
-                    dfs(i, j, board, vis);
+void solve(vector<vector<char>>& board) {
+    int m = board.size();
+    int n = board[0].size();
+    
+    vector<vector<int>> vis(m, vector<int>(n, 0));
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || i==m-1 || j==0 || j==n-1){
+                if(board[i][j]=='O'){
+                    cout<<"hello"<<endl;
+                    dfs(i,j, vis, board);
                 }
             }
         }
     }
-    vector<vector<char>> ans(n,vector<char>(m));
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(vis[i][j]==1){
-                ans[i][j]='O';
-            }
-            else{
-                ans[i][j]='X';
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<vis[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    vector<vector<char>> ans(m, vector<char>(n, 'X'));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(board[i][j]=='O' && vis[i][j] == 0){
+                board[i][j] = 'X';
             }
         }
     }
-    board=ans;
 }
 };
