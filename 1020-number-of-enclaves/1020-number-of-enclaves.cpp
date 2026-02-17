fd18c1ce6cb3395447f155a3b2dcbe5fb1c529c8
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-18 01:37:51 +0530

    Time: 202 ms (5.69%), Space: 59.1 MB (5.01%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>&grid, vector<vector<int>> &vis){
-    vis[r][c]=1;
-    int n=grid.size();
-    int m=grid[0].size();
-    vector<int> drow={-1,0,1,0};
-    vector<int> dcol={0,1,0,-1};
+    void dfs(int x, int y, vector<vector<int>> &vis, vector<vector<int>> &grid){
+    int m = grid.size();
+    int n = grid[0].size();
+    vis[x][y] = 1;
+    vector<int> dx = {-1, 0, 1, 0};
+    vector<int> dy = {0, 1, 0, -1};
     for(int i=0;i<4;i++){
-        int nrow=r+drow[i];
-        int ncol=c+dcol[i];
-        //check if valid and perform further dfs
-        if(nrow>=0&&ncol>=0&&nrow<n&&ncol<m&&vis[nrow][ncol]!=1&&grid[nrow][ncol]==1){
-            dfs(nrow,ncol,grid,vis);
+        int nx = x+ dx[i];
+        int ny = y+ dy[i];
+        if(nx>=0 && nx<m && ny>=0 && ny<n && vis[nx][ny]==0 && grid[nx][ny]==1){
+            dfs(nx, ny, vis, grid);
         }
     }
 }
 
 int numEnclaves(vector<vector<int>>& grid) {
-    int n=grid.size();
-    int m=grid[0].size();
-    vector<vector<int>> vis(n,vector<int>(m,0));
-    //only start dfs for the boundary elements
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(i==0 || j==0 || i==n-1 || j==m-1){
-                if(grid[i][j]==1 && vis[i][j]!=1){
-                    dfs(i,j,grid,vis);
+    int m = grid.size();
+    int n = grid[0].size();
+    
+    vector<vector<int>> vis(m, vector<int>(n,0));
+    for(int i=0;i<m;i++){
+        for(int  j=0;j<n;j++){
+            if(i==0 || i==m-1 || j==0 || j==n-1){
+                if(grid[i][j] == 1){
+                    dfs(i, j, vis, grid);
                 }
             }
         }
     }
 
     int cnt=0;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(grid[i][j]==1 && vis[i][j]==0){
-                cnt++;
-            }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]==1 && vis[i][j]==0)cnt++;
         }
     }
     return cnt;
