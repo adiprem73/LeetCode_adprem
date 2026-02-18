48f3a15dd1ff366993e21d41ad66b67c01229646
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-18 13:40:32 +0530

    Time: 13 ms (41.33%), Space: 98 MB (39.95%) - LeetHub

---
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    void dfs(int x, int y, vector<vector<int>>& grid, int &sum){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> dx={-1,0,1,0};
+        vector<int> dy={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]!=0){
+                sum+=grid[nx][ny];
+                grid[nx][ny]=0;
+                dfs(nx, ny, grid, sum);
+            }
+        }
+    }
+
+public:
+    int findMaxFish(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans =0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=0){
+                    int sum= grid[i][j];
+                    grid[i][j]=0;
+                    dfs(i, j, grid, sum);
+                    ans = max(ans, sum);
+                }
+            }
+        }
+        return ans;
+    }
+};
