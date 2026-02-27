b8bc5f35668dcb8372a07318b88e47cec73df324
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-27 10:59:29 +0530

    Time: 91 ms (33.74%), Space: 47.9 MB (26.79%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -2,45 +2,34 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>> &times, int n, int k)
 {
-    vector<vector<pair<int, int>>> adj(n+1);
-    vector<int> vis(n+1, 1e9);
-    
-    for (auto it : times)
-    {
-        adj[it[0]].push_back({it[1], it[2]});
+    vector<vector<pair<int,int>>> adj(n+1);
+    for(auto it: times){
+        adj[it[0]].push_back({it[1], it[2]}); // {node, time}
     }
-
-    queue<pair<int, int>> q; // time,node
-    q.push({0, k});
-    vis[k]=0;
-
-
-    while (!q.empty())
-    {
-        auto temp = q.front();
-        int node = temp.second;
-        int time = temp.first;
-        q.pop();
-
-        for (auto it : adj[node])
-        {
-            int adjNode = it.first;
-            int edgeWeight = it.second;
-            if (time + edgeWeight < vis[adjNode])
-            {
-                vis[adjNode] = time + edgeWeight;
-                q.push({time + edgeWeight, adjNode});
+    // the adjacency list is ready now
+    vector<int> time(n+1, 1e9);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+    pq.push({0, k}); //{time, node}
+    time[k]= 0;
+    while(!pq.empty()){
+        auto [timeTaken, node] = pq.top();
+        pq.pop();
+        for(auto neigh : adj[node]){
+            int wt = neigh.second;
+            int nd = neigh.first;
+            if(time[nd] > timeTaken+wt){
+                time[nd] =  timeTaken+wt; // relaxed
+                pq.push({timeTaken+wt, nd});
             }
         }
     }
-
-    int minTime= 0;
-    for(int i=1;i<=n;i++){
-        minTime=max(minTime, vis[i]);
+    int sum =0;
+    for(int i=1;i<n+1;i++){
+        if(time[i] == 1e9)return -1;
+        else{
+            sum = max(sum, time[i]);
+        }
     }
-
-
-    if(minTime==1e9)return -1;
-    return minTime;
+    return sum;
 }
 };
