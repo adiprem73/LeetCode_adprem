d0cceda1899790c3088a95742499e9b61bc95cd6
Aditya Prem <aditya.prem2003@gmail.com>
2026-02-27 10:44:08 +0530

    Time: 8 ms (22.26%), Space: 18.7 MB (25.66%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
+{
     vector<vector<pair<int,int>>> adj(n);
+    vector<vector<int>> costs(n, vector<int>(k+2, 1e9));
     for(auto it: flights){
-        adj[it[0]].push_back({it[1],it[2]});
+        adj[it[0]].push_back({it[1], it[2]}); 
     }
+    // we have made the matrix
+    priority_queue < pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+    pq.push({0, {0, src}});  // (cost,(steps, city))
+    costs[src][0] = 0;
+    while(!pq.empty()){
+        auto [cost, temp] = pq.top();
+        int city = temp.second;
+        int steps = temp.first;
+        pq.pop();
 
-    queue<pair<int,pair<int,int>>> q;
-    q.push({0,{src,0}});   //stops, node, distance or cost
-    //distance array
-    vector<int> distance(n,1e9);
-    distance[src]=0;
+        if (steps > k+1)
+            continue;
+        if (city == dst)
+        {
+            return cost;
+        }
 
-
-    while(!q.empty()){
-        auto it= q.front();
-        q.pop();
-        int stops= it.first;
-        int node= it.second.first;
-        int dist= it.second.second;
-
-        if(stops>k)continue;
-
-        for(auto iter: adj[node]){
-            int adjNode= iter.first;
-            int edgeWeight= iter.second;
-
-            if(dist+edgeWeight< distance[adjNode]){
-                distance[adjNode]= dist+edgeWeight;
-                q.push({stops+1,{adjNode, dist+edgeWeight}});
+        for(auto it: adj[city]){
+            auto extraCost = it.second;
+            auto stop = it.first;
+            if(cost+extraCost < costs[stop][steps]){
+                pq.push({cost+extraCost, {steps+1,stop}});
+                costs[stop][steps] = cost+extraCost;
             }
         }
     }
-
-
-    if(distance[dst]==1e9)return -1;
-    return distance[dst];
+    return -1;
 }
 };
