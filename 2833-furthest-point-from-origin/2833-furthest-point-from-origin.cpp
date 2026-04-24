f91dab54c063ebedde56c2b518e656f746df9169
Aditya Prem <aditya.prem2003@gmail.com>
2026-04-24 19:31:16 +0530

    Time: 1 ms (12.55%), Space: 9.8 MB (6.23%) - LeetHub

---
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+    map<char,int> mp;
+    mp['L']=0;
+    mp['R']=0;
+    mp['_']=0;
+    for(auto ch: moves){
+        mp[ch]++;
+    }       
+    if(mp['L']>=mp['R']){
+        return mp['L']-mp['R']+mp['_'];
+    }
+    if (mp['L'] < mp['R'])
+    {
+        return mp['R'] - mp['L'] + mp['_'];
+    }
+    // mprint(mp);
+    return 0;
+}
+};
