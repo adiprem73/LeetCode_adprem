1681ca2da818589100d6b268846abc30fece3bd1
Aditya Prem <aditya.prem2003@gmail.com>
2026-04-24 21:56:05 +0530

    Time: 0 ms (100%), Space: 21.3 MB (99.64%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-
+    int maximumCount(vector<int> &nums)
+{
     int n = nums.size();
+    int s=0;
+    int e = n-1;
+    int ans = n; // by default for not found
 
-    int l = 0, r = n-1;
-
-    // first >=0
-    while(l < r){
-        int mid = l + (r-l)/2;
-        if(nums[mid] < 0)
-            l = mid + 1;
-        else
-            r = mid;
+    // lower bound
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        // cout<<mid<<endl;
+        if(nums[mid]>=0){
+            ans = mid;
+            e = mid -1;
+        }else{
+            s = mid +1;
+        }
     }
 
-    int negatives = (nums[l] < 0) ? l + 1 : l;
-
-    l = 0, r = n-1;
-
-    // first >0
-    while(l < r){
-        int mid = l + (r-l)/2;
-        if(nums[mid] <= 0)
-            l = mid + 1;
-        else
-            r = mid;
+    // upper bound
+    s = 0;
+    e = n - 1;
+    int ans2= n;
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        if(nums[mid]>0){
+            ans2 = mid;
+            e = mid-1;
+        }else{
+            s = mid+1;
+        }
     }
 
-    int positives = (nums[l] > 0) ? n - l : 0;
-
-    return max(negatives, positives);
+    // cout<<s<<endl;
+    // cout<<n-s<<endl;
+    // cout<<ans<<endl;
+    // cout<<ans2<<endl;
+    // int neg = ans;
+    // int pos = n- ans2;
+    return max(ans, n-ans2);
+    // cout<<s<<endl;
 }
 };
