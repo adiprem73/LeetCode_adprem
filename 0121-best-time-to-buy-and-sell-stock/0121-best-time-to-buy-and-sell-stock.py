b76dcc698d56877bbf5817552f920bacc456b662
Aditya Prem <aditya.prem2003@gmail.com>
2026-04-26 22:34:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def maxProfit(self, prices):
-        l=0
-        r=1
-        max_profit=0
-        while(r<len(prices)):
-            if(prices[l]<prices[r]):
-                max_profit=max(max_profit, prices[r]-prices[l])
-            else:
-                l=r
-            r+=1
-            
+        n = len(prices)
+        max_profit = 0
+
+        for i in range(n):  # buy day
+            for j in range(i + 1, n):  # sell day (must be after buy)
+                profit = prices[j] - prices[i]
+                max_profit = max(max_profit, profit)
+
         return max_profit
-        
