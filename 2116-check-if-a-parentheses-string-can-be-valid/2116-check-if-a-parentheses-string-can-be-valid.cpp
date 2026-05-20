f839f84517c9eaec375017742e24abfb11306858
Aditya Prem <aditya.prem2003@gmail.com>
2026-05-20 16:00:31 +0530

    Time: 15 ms (38.73%), Space: 30.1 MB (50%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -3,31 +3,27 @@ public:
     bool canBeValid(string s, string locked)
 {
     int n = s.length();
-    if(n%2 != 0)return false;
-
-    stack<int> open, openClose;
-
+    if(n%2!=0)return false;
+    int open=0;
     for(int i=0;i<n;i++){
-        if(locked[i] == '0'){
-            openClose.push(i);
-        }else if(s[i] == '('){
-            open.push(i);
+        if(s[i] == '(' || locked[i] == '0'){
+            open++;
         }else if(s[i] == ')'){
-            if(!open.empty()){
-                open.pop();
-            }else if(openClose.empty() == false){
-                openClose.pop();
-            }else{
-                return false;
-            }
+            open--;
         }
+        if(open<0)return false;
     }
 
-    while(!open.empty() && !openClose.empty() && open.top() < openClose.top()){
-        open.pop();
-        openClose.pop();
+    int close =0;
+    for(int i=n-1;i>=0;i--){
+        if(s[i] == ')' || locked[i] == '0'){
+            close++;
+        }
+        else if(s[i] == '('){
+            close--;
+        }
+        if(close<0)return false;
     }
-
-    return open.empty();
+    return true;
 }
 };
