883b534d2208488ce890d2ecc76074dbb86e0a07
Aditya Prem <aditya.prem2003@gmail.com>
2026-05-23 15:12:15 +0530

    Time: 226 ms (61.53%), Space: 11 MB (46.75%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,49 +1,38 @@
 class Solution {
 public:
-    bool dfs(int r, int c, int &m, int &n, string &curr, vector<vector<char>> &board, string word, vector<vector<int>>& vis){
-    int dx[]={-1,0,1,0};
-    int dy[]={0,1,0,-1};
+    bool dfs(int r, int c, int ind, vector<vector<char>>& board, string &word, vector<vector<int>>& vis){
+    vis[r][c]= 1;
 
-    vis[r][c] = 1;
-    curr.push_back(board[r][c]);
-    // cout<<curr<<endl;
-    // base case
-    if(curr==word){
-        vis[r][c] = -1;
-        curr.pop_back();
+    int m = board.size();
+    int n = board[0].size();
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, 1, 0, -1};
+    // base cases
+    if(ind == word.size()-1){
+        vis[r][c] = -1; // backtracking
         return true;
-    }
-
-    // pruning
-    if (curr != word.substr(0, curr.length()))
-    {
-        vis[r][c] = -1;
-        curr.pop_back();
-        return false;
     }
 
     for(int i=0;i<4;i++){
         int nr = r+dx[i];
         int nc = c+dy[i];
-        if(nr>=0 && nr<m && nc>=0 && nc<n){
-            if(vis[nr][nc]==-1){
-                
-                if(dfs(nr, nc, m, n, curr, board, word, vis)){
-                    vis[r][c] = -1;
-                    curr.pop_back();
-                    return true;
-                }
-                
+        if(nr>=0 && nr<m && nc>=0 && nc<n && vis[nr][nc]==-1 && board[nr][nc] == word[ind+1]){
+            
+            if(dfs(nr, nc, ind+1, board, word, vis)){
+                vis[r][c] = -1; // backtracking
+                return true;
             }
+
+            // 
         }
     }
 
-    // backtrack
-    vis[r][c] = -1;
-    curr.pop_back();
+    // backtracking
+    vis[r][c]=-1;
 
     return false;
 }
+
 
 bool exist(vector<vector<char>> &board, string word)
 {
@@ -52,10 +41,11 @@ bool exist(vector<vector<char>> &board, string word)
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             vector<vector<int>> vis(m, vector<int>(n, -1));
-            string curr = "";
 
-            if(dfs(i, j, m, n, curr, board, word, vis)){
-                return true;
+            if(word[0] == board[i][j]){
+                if(dfs(i, j, 0, board, word, vis)){
+                    return true;
+                }
             }
         }
     }
