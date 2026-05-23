2e09913f9ac276073e6d2a877e53b4e8b13b0e50
Aditya Prem <aditya.prem2003@gmail.com>
2026-05-23 15:01:44 +0530

    Time: 1065 ms (7.19%), Space: 12 MB (9.63%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,69 +1,65 @@
 class Solution {
 public:
-    bool func(int ind, int x, int y, vector<vector<char>> &board, string word)
-{
+    bool dfs(int r, int c, int &m, int &n, string &curr, vector<vector<char>> &board, string word, vector<vector<int>>& vis){
+    int dx[]={-1,0,1,0};
+    int dy[]={0,1,0,-1};
 
-
-    if (ind == word.size() - 1)
-    {
-        // cout << "executed" << endl;
+    vis[r][c] = 1;
+    curr.push_back(board[r][c]);
+    // cout<<curr<<endl;
+    // base case
+    if(curr==word){
+        vis[r][c] = -1;
+        curr.pop_back();
         return true;
     }
 
-    // to prevent same cell being counted twise we can mark the visited as '#'
-    char temp = board[x][y];
-    board[x][y]='#';
-
-
-    int n = board.size();    // no. of rows
-    int m = board[0].size(); // no. of columns
-    // cout<<"no. of rows : "<<n<<endl;
-    // cout<<"no. of cols : "<<m<<endl;
-    vector<int> dx = {0, 1, 0, -1};
-    vector<int> dy = {1, 0, -1, 0};
-    for (int i = 0; i < 4; i++)
+    // pruning
+    if (curr != word.substr(0, curr.length()))
     {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (nx >= 0 && nx < n && ny >= 0 && ny < m)
-        {
-            if (word[ind+1] == board[nx][ny])
-            {
-                if (func(ind + 1, nx, ny, board, word))
-                {
-                    board[x][y]= temp; // backtracking before returning
+        vis[r][c] = -1;
+        curr.pop_back();
+        return false;
+    }
+
+    for(int i=0;i<4;i++){
+        int nr = r+dx[i];
+        int nc = c+dy[i];
+        if(nr>=0 && nr<m && nc>=0 && nc<n){
+            if(vis[nr][nc]==-1){
+                
+                if(dfs(nr, nc, m, n, curr, board, word, vis)){
+                    vis[r][c] = -1;
+                    curr.pop_back();
                     return true;
                 }
+                
             }
         }
     }
-    board[x][y]= temp; //backtracking... 
+
+    // backtrack
+    vis[r][c] = -1;
+    curr.pop_back();
+
     return false;
 }
 
 bool exist(vector<vector<char>> &board, string word)
 {
-    bool flag;
+    int m = board.size();
+    int n = board[0].size();
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            vector<vector<int>> vis(m, vector<int>(n, -1));
+            string curr = "";
 
-    for (int i = 0; i < board.size(); i++)
-    {
-        for (int j = 0; j < board[0].size(); j++)
-        {
-
-            if (word[0] == board[i][j])
-            {
-                // cout << "x : " << i << endl
-                //      << "y : " << j << endl;
-                // cout<<"character is : "<<word[ind]<<endl;
-                flag = func(0, i, j, board, word);
-                if (flag == true)
-                {
-                    return flag;
-                }
+            if(dfs(i, j, m, n, curr, board, word, vis)){
+                return true;
             }
         }
     }
-
-    return flag;
+    return false;
+    
 }
 };
