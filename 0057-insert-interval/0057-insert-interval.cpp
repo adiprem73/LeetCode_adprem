2b713dea418af63dddb8aa9d848d17e100c8fed0
Aditya Prem <aditya.prem2003@gmail.com>
2026-05-31 11:41:50 +0530

    Time: 3 ms (29.88%), Space: 21.5 MB (94.19%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-    int n = intervals.size();
-    // if(n==0){
-    //     intervals.push_back(newInterval);
-    //     return intervals;
-    // }
-    // for(int i=0;i<n;i++){
-    //     if(intervals[i][0]<=newInterval[0] && intervals[i][1]>=newInterval[0]){
-    //         intervals[i][1] = max(intervals[i][1], newInterval[1]);
-    //     }
-    //     else{
-    //         intervals.push_back(newInterval);
-    //         sort(intervals.begin(), intervals.end());
-    //     }
-    // }   
-
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
     intervals.push_back(newInterval);
+    // now we first need to sort the interval array
     sort(intervals.begin(), intervals.end());
-    //now we need to fix the issue of overlapping intervals
-    for(int i=0;i<intervals.size()-1;i++){
-        if(intervals[i][1]>=intervals[i+1][0]){
-            intervals[i][1]= max(intervals[i][1], intervals[i+1][1]);
-            intervals.erase(intervals.begin()+i+1);
-            i--;
+    int n = intervals.size();
+    vector<vector<int>> ans;
+    ans.push_back(intervals[0]);
+    for(int i=1;i<n;i++){
+        if(ans.back()[1]>=intervals[i][0]){
+            ans.back()[1]= max( ans.back()[1] ,intervals[i][1]);
+        }else{
+            ans.push_back(intervals[i]);
         }
-    }  
-    return intervals;
+    }
+    return ans;
 }
 };
