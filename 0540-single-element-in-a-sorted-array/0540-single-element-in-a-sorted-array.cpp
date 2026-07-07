9b9e3c06156b4575b63becd05682ea20c0efd961
Aditya Prem <aditya.prem2003@gmail.com>
2026-07-07 11:27:21 +0530

    Time: 0 ms (100%), Space: 26.2 MB (68.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-
-    int n=nums.size();
-
-    //resolving the edge cases
-    if(n==1)  return nums[0];
-    if(nums[0]!=nums[1]){
-        return nums[0];
-    }
-    if(nums[n-1]!=nums[n-2]){
-        return nums[n-1];
-    }
-
-    int s=0,e=n-1;
+    int singleNonDuplicate(vector<int> &nums)
+{
+    int n = nums.size();
+    int s =0;
+    int e = n-1;
+    if(n==1)return nums[0];
+    if(nums[0]!=nums[1])return nums[0];
+    if(nums[n-1]!=nums[n-2])return nums[n-1];
     while(s<=e){
-        int mid=s+(e-s)/2;
-        // cout<<"hi";
-        if(nums[mid]!=nums[mid+1]&&nums[mid]!=nums[mid-1]){
+        int mid = s+(e-s)/2;
+        if(  nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
             return nums[mid];
         }
-
-
-        //left side eleimination condition
-        if(((mid%2==0)&&nums[mid]==nums[mid+1])||((mid%2==1)&&nums[mid]==nums[mid-1])){
-            s=mid+1;
+        else if(mid+1<n &&  nums[mid]==nums[mid+1]){
+            if(mid%2==0){ // 1st occurrence is even
+                s=mid + 1;
+            }else{ // 1st occurrence is odd
+                e=mid - 1;
+            }
         }
-
-        //right side elimination condition
         else{
-            e=mid-1;
+            if(mid%2==0){ //2nd occurrence is even
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
         }
     }
-    return -1;
-
-
+    return 0;
 }
 };
