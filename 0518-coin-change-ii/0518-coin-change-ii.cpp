e2ec156c73cf64bd2399fb853def9f5dad5237c4
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-01 02:09:33 +0530

    Time: 43 ms (29.88%), Space: 36.9 MB (27.56%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-int func(int ind, int amount, vector<int> &coins, vector<vector<int>>& dp)
+    int func(int ind, int amount, vector<int> &coins, vector<vector<int>>& dp)
 {
     // base case
-    if (amount == 0)
-        return 1;
-    if (ind < 0)
+    int n = coins.size();
+    if(ind >= n){
+        if(amount == 0 )return 1;
         return 0;
-    if (amount < 0)
-        return 0;
-
-    if(dp[ind][amount]!=-1)return dp[ind][amount];
-    int notPick = func(ind - 1, amount, coins, dp);
-    int pick = 0;
-    if (coins[ind] <= amount)
-    {
-        pick = func(ind, amount - coins[ind], coins, dp);
     }
-    return dp[ind][amount]=pick + notPick;
+
+    // dp check
+    if(dp[ind][amount] != -1){
+        return dp[ind][amount];
+    }
+
+    int take = 0;
+    // this is a knapsack problem. we will just (take and stay) and (not take and move ahead) our way through this problem
+    int notTake = func(ind+1, amount, coins, dp);
+    if(coins[ind]<=amount){
+        take = func(ind, amount-coins[ind], coins, dp);
+    }
+    return dp[ind][amount] = take + notTake;
 }
 
 int change(int amount, vector<int> &coins)
 {
     int n = coins.size();
-    vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-    return func(n - 1, amount, coins, dp);
+    vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+    return func(0, amount, coins, dp);
 }
 };
