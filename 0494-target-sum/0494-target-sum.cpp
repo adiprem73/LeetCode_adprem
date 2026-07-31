e5f24ada1eabb8e553257f7ba7e137ede46d1cfa
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-01 00:54:01 +0530

    Time: 951 ms (5.1%), Space: 11.8 MB (79.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-int findTargetSumWays(vector<int>& nums, int target) {
-    int n=nums.size();
-    int totalSum=accumulate(nums.begin(), nums.end(),0);
-
-    if(abs(target)>totalSum) return 0;
-    vector<vector<int>> dp(n, vector<int>(2* totalSum + 1, -1));
-
-    for(int j=-totalSum ; j<=totalSum ; j++){
-        if (nums[0]==0 && j==0) dp[0][j+totalSum]=2;
-        else if (j==nums[0] || j==-nums[0]) dp[0][j+totalSum]=1;
-        else dp[0][j+ totalSum] = 0;
-    }
-    
-
-    for(int i=1;i<n;i++){
-        for(int j=-totalSum;j<=totalSum;j++){
-            int plus=(j-nums[i] >= -totalSum)? dp[i-1][j- nums[i]+totalSum] : 0;
-
-            int minus=(j+nums[i] <= totalSum)? dp[i-1][j+ nums[i]+totalSum] : 0;
-
-            dp[i][j+totalSum]=plus + minus;
-        }
+    int func(int ind, int target, vector<int>& nums){
+    int n = nums.size();
+    // base case
+    if(ind == n){
+        if(target == 0)return 1;
+        return 0;
     }
 
-    return dp[n-1][target+totalSum];
 
+    // additon 
+    int add = func(ind+1, target-nums[ind], nums);
+
+    // subtraction
+    int sub = func(ind+1, target+nums[ind], nums);
+
+    return add + sub;
+}
+
+int findTargetSumWays(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    return func(0, target, nums);
 }
 };
