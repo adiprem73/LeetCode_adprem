5da5f9a0ffb72f86466e252d2bab73e0421344de
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-05 00:19:43 +0530

    Time: 10 ms (23.85%), Space: 20.3 MB (16.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
-    bool detectCycle(vector<vector<int>>& adj){
-    int n=adj.size();
-    //make the indegree array
-    vector<int> indegree(n,0);
-    for(int i=0;i<n;i++){
-        for(auto it:adj[i]){
-            indegree[it]++;
-        }
-    }
-    
-    //start the bfs from the node with indegree 0
-    queue<int> q;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-            q.push(i);
-        }
-    }
 
-    //start the bfs and the topological sorting
-    vector<int> topo;
-    while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        topo.push_back(node);
-        for(auto it:adj[node]){
-            indegree[it]--;
-            if(indegree[it]==0){
-                q.push(it);
-            }
+    bool dfs(int node, vector<int> &pathVis, vector<int> &vis, int n,  vector<vector<int>>& adj){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto adjNode : adj[node]){
+            if(vis[adjNode] == 0){ // it is already visited
+                if(dfs(adjNode, pathVis, vis, n, adj) == false){
+                    return false;
+                }
+            }else if(pathVis[adjNode] == 1){
+                return false;
+            }         
         }
-    }
-    if(topo.size()<n){
+        pathVis[node]=0;
         return true;
     }
-    return false;
-}
 
-bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    vector<vector<int>>adj(numCourses);
-    for(auto it: prerequisites){
-        adj[it[1]].push_back(it[0]);
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+        vector<vector<int>> adj(numCourses);
+        for(auto it: prerequisites){
+            adj[it[0]].push_back(it[1]);
+        }
+
+        // for this we need to detect cycles in an directed grpahs
+        // there are two ways to do this: 
+        // 1. using dfs(vis and pathVis)
+        // 2. using Kahn's Algorithm
+
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
+        for(int i=0;i<numCourses;i++){
+            if(vis[i] == 0){
+                if(dfs(i, pathVis, vis, numCourses, adj) == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+
     }
-    // for(int i=0;i<numCourses;i++){
-    //     vprint(adj[i]);
-    // }
-    //the courses must not be cyclic in order to be completed, means there shouldnt be any cycle in hte entire graph of the directed course
-    if(detectCycle(adj)==true)return false;
-    return true;
-}
 };
