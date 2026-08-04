9512e48bf3535bcb14b5e5ae4ea1e8fb90bfa23b
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-04 23:28:12 +0530

    Time: 0 ms (100%), Space: 18.3 MB (31.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
-{
-    int m = image.size();
-    int n = image[0].size();
-    // let us do this quesitons by using BFS
-    queue<pair<int,int>>q;
-    q.push({sr,sc});
-    vector<int> dx = {-1, 0, 1 , 0};
-    vector<int> dy = {0, 1, 0, -1};
-    vector<vector<bool>> visited(m, vector<bool>(n,false));
-    visited[sr][sc]= true;
-    while(!q.empty()){
-        // we mustmove in all four directions 
-        auto [x, y] = q.front();
-        q.pop();
-        int prevcolor = image[x][y];
-        image[x][y]= color;
-        // we do not need to perform layer by layer bfs so we will jsut do normal bfs
-        for(int i=0;i<4;i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(nx>=0 && nx<m && ny>=0 && ny<n && visited[nx][ny]==false && image[nx][ny] == prevcolor){
-                q.push({nx, ny});
-                visited[nx][ny] = true;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
+        int conditionColor = image[sr][sc];
+        if(conditionColor == color)return image;
+        // we will use a BFS approach
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc] = color;
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        while(!q.empty()){
+            int size= q.size();
+            for(int i=0;i<size;i++){
+                auto [r,c] = q.front();
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nr = r+dr[i];
+                    int nc = c+dc[i];
+                    if(nr>=0 && nr<m && nc>=0 && nc<n){
+                        if(image[nr][nc] == conditionColor){
+                            image[nr][nc] = color;
+                            q.push({nr,nc});
+                        }
+                    }
+                }
             }
         }
+        return image;
     }
-    return image;
-}
 };
