36141cc37bb349f151e59bbf9738ad7310612c28
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-04 22:32:58 +0530

    Time: 0 ms (100%), Space: 19.5 MB (60.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    void dfs(int u, vector<vector<int>> &adj, vector<bool> & visited){
-    // mark this visited
-    visited[u]= true;
-    for(auto neighbours: adj[u]){ // traverse through all the neighbouring nodes
-        if(visited[neighbours] == false){
-            dfs(neighbours, adj, visited);
-        }
-    }
-}
-
-int findCircleNum(vector<vector<int>> &isConnected)
-{
-    // the very first thing to do will be to convert the graph from matrix form to vector form so that we can perform  our traversals easily
-    int n = isConnected.size();
-    vector<vector<int>> adj(n);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(isConnected[i][j]==1){
-                adj[i].push_back(j);
-                adj[j].push_back(i);
+    void dfs(int city, vector<int>& vis, vector<vector<int>> & isConnected){
+        int n = isConnected.size();
+        vis[city]=1; //visited marked
+        for(int i=0;i<n;i++){
+            if(isConnected[city][i] == 1 && vis[i] == 0){
+                dfs(i, vis, isConnected);
             }
         }
     }
 
-    // we will maintain a count variable to count the number of times dfs gets called from this main function: this will only give the correct number of provinces
-    int cnt=0;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        int count=0;
+        vector<int> vis(n);
 
-
-    // now first we will try for a dfs approach
-    // for this we will need a visited array as well so that we do not go aorund in circles
-    vector<bool> visited(n);
-    for(int i=0;i<n;i++){
-        if(visited[i]== false){
-            dfs(i, adj, visited);
-            cnt++;
+        for(int i=0;i<n;i++){
+            if(vis[i] == 0){
+                cout<<i<<endl;
+                count++;
+                dfs(i, vis, isConnected);
+            }
         }
+        return count;
     }
-    return cnt;
-}
 };
