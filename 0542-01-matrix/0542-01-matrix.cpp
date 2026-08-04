0c491591fa309b43aea70c1ae46ddf08da185198
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-05 01:21:09 +0530

    Time: 16 ms (66.61%), Space: 37.3 MB (51.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
-{
-    // we will iterate from every zero and keep on updating the distance of each cell from it
-    queue<pair<int,int>>q;
-    int m = mat.size();
-    int n = mat[0].size();
-    // vector<vector<int>> ans(m, vector<int>(n, 1e9));
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j]==0){
-                q.push({i,j});
-            }else{
-                mat[i][j]= 1e9;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        // i will just use bfs
+        queue<pair<int,int>> q; //(row, col)
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>>vis(m, vector<int>(n, 0));
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                }
             }
         }
-    }
-    vector<int> dx={-1, 0, 1, 0};
-    vector<int> dy={0, 1, 0, -1};
-    while(!q.empty()){
-        auto node = q.front();
-        q.pop();
-        auto [x,y] = node;
-        for(int i=0;i<4;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx>=0 && nx<m && ny>=0 && ny<n && mat[nx][ny]> mat[x][y]+1){
-                q.push({nx,ny});
-                mat[nx][ny] = mat[x][y]+1;
+        int dr[]= {-1,0,1,0};
+        int dc[]= {0,1,0,-1};
+        int steps=0;
+        
+
+        while(!q.empty()){
+            int size = q.size();
+            steps++;
+            for(int i=0;i<size;i++){
+                auto [r,c] = q.front();
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int nr = r+dr[j];
+                    int nc = c+dc[j];
+                    // checking
+                    if(nr>=0 && nr<m && nc>=0 && nc<n){
+                        if(mat[nr][nc] != 0 && vis[nr][nc] == 0){
+                            vis[nr][nc]=1;
+                            mat[nr][nc]= steps;
+                            q.push({nr,nc});
+                        }
+                    }
+                }
             }
         }
+        return mat;
     }
-    return mat;
-}
 };
