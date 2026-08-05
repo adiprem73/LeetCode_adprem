4752ece026b3be763ddd29075e3a06161d599c76
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-05 15:44:06 +0530

    Time: 78 ms (5.32%), Space: 70.5 MB (5.14%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-    int n=graph.size();
-    vector<int> indegree(n);
-    vector<vector<int>> rgraph(n);
+        // to solve this problem, we just need to reverse the edges and then do topo Sort
+        int n= graph.size();
+        vector<vector<int>> adj(n);
 
-    //reversal and indegreeof the reversed grpah is calculated
-    for(int i=0;i<n;i++){
-        // i->it
-        // it->i
-        for(auto it: graph[i]){
-            rgraph[it].push_back(i);
-            indegree[i]++;
-        }
-    }
-    queue<int> q;
-    vector<int> safenode;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-            q.push(i);
-        }
-    }
-
-    while(!q.empty()){
-        int node=q.front();
-        q.pop();
-        safenode.push_back(node);
-        for(auto it: rgraph[node]){
-            indegree[it]--;
-            if(indegree[it]==0){
-                q.push(it);
+        for(int i=0;i<n;i++){
+            for(auto it: graph[i]){
+                adj[it].push_back(i);
             }
         }
+
+        // now that the edges have been reversed, we need to do topo sort
+        vector<int> indegrees(n,0);
+        for(auto it: adj){
+            for(auto itr: it){
+                indegrees[itr]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(indegrees[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto adjNode : adj[node]){
+                indegrees[adjNode]--;
+                if(indegrees[adjNode] == 0){
+                    q.push(adjNode);
+                }
+            }
+        }
+
+        sort(topo.begin(), topo.end());
+return topo;
+
+
     }
-    sort(safenode.begin(),safenode.end());
-    return safenode;
-}
 };
