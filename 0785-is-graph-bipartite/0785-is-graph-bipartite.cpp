f5ae3731b851c94bc760526869e3805a47e5f531
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-05 13:30:31 +0530

    Time: 0 ms (100%), Space: 17.3 MB (93.1%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>> &graph)
-{
-    int n = graph.size();
-    vector<int> color(n ,-1); // -1: shows that the node is not yet colored
-    queue<int> q;
-    q.push(0);
-    color[0]=0;
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        for(auto neigh: graph[node]){
-            if(color[neigh] == -1){
-                color[neigh]= !color[node];
-                q.push(neigh);
-            }else{
-                if(color[neigh] == color[node]){
+
+    bool dfs(int node, int color, vector<int> & colors, vector<vector<int>>&graph){
+        for(int adjNode: graph[node]){
+            if(colors[adjNode]==-1){
+                colors[adjNode] = 1-color;
+                if(dfs(adjNode, 1-color, colors, graph)==false){
                     return false;
                 }
+            }else{
+                if(colors[adjNode] == color)return false;
             }
         }
+        return true;
     }
-    return true;
-}
+
+// if graph has odd length cycle: it cannot be bipartite
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> colors(n, -1);
+        for(int i=0;i<n;i++){
+            if(colors[i]==-1){
+                if(dfs(i, 0, colors, graph) == false)return false;
+            }
+        }
+        return true;
+    }
 };
