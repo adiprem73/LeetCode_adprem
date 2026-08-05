b6be9bf47f16ab1db89807245cd5f3e4763e1529
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-06 00:28:18 +0530

    Time: 3 ms (61.55%), Space: 18.4 MB (31.55%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
-{
-    vector<vector<pair<int,int>>> adj(n);
-    vector<vector<int>> costs(n, vector<int>(k+2, 1e9));
-    for(auto it: flights){
-        adj[it[0]].push_back({it[1], it[2]}); 
-    }
-    // we have made the matrix
-    priority_queue < pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-    pq.push({0, {0, src}});  // (cost,(steps, city))
-    costs[src][0] = 0;
-    while(!pq.empty()){
-        auto [cost, temp] = pq.top();
-        int city = temp.second;
-        int steps = temp.first;
-        pq.pop();
-
-        if (steps > k+1)
-            continue;
-        if (city == dst)
-        {
-            return cost;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it: flights){
+            adj[it[0]].push_back({it[1], it[2]});
         }
 
-        for(auto it: adj[city]){
-            auto extraCost = it.second;
-            auto stop = it.first;
-            if(cost+extraCost < costs[stop][steps]){
-                pq.push({cost+extraCost, {steps+1,stop}});
-                costs[stop][steps] = cost+extraCost;
+        queue<pair<int,pair<int,int>>> q;
+        q.push({0,{src,0}});
+        vector<int> dist(n, 1e9);
+        dist[src]=0;
+
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+
+            int stops = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+
+            if(stops>k)continue;
+            for(auto iter: adj[node]){
+                int adjNode = iter.first;
+                int edW = iter.second;
+
+                if(cost+edW < dist[adjNode] && stops <=k){
+                    dist[adjNode]= cost + edW;
+                    q.push({stops+1 , {adjNode, cost+edW}});
+                }
             }
         }
+
+        if(dist[dst] == 1e9)return -1;
+
+        return dist[dst];
     }
-    return -1;
-}
 };
