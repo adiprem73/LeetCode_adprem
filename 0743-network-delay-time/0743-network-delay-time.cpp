ce86830bb8f543df64a3d072500432c917c0162f
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-06 00:46:08 +0530

    Time: 91 ms (31.51%), Space: 48 MB (17.53%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>> &times, int n, int k)
-{
-    vector<vector<pair<int,int>>> adj(n+1);
-    for(auto it: times){
-        adj[it[0]].push_back({it[1], it[2]}); // {node, time}
-    }
-    // the adjacency list is ready now
-    vector<int> time(n+1, 1e9);
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-    pq.push({0, k}); //{time, node}
-    time[k]= 0;
-    while(!pq.empty()){
-        auto [timeTaken, node] = pq.top();
-        pq.pop();
-        for(auto neigh : adj[node]){
-            int wt = neigh.second;
-            int nd = neigh.first;
-            if(time[nd] > timeTaken+wt){
-                time[nd] =  timeTaken+wt; // relaxed
-                pq.push({timeTaken+wt, nd});
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto it: times){
+            adj[it[0]].push_back({it[1], it[2]});
+        }
+        // now we will apply standard djikstra
+        vector<int> dist(n+1, 1e9);
+        dist[k] = 0;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>>pq;
+        pq.push({0,k}); // (cost, node)
+
+        while(!pq.empty()){
+            auto [cost, node] = pq.top();
+            pq.pop();
+            for(auto iter: adj[node]){
+                int adjNode = iter.first;
+                int edW = iter.second;
+                if(edW + cost < dist[adjNode]){
+                    dist[adjNode] = edW+cost;
+                    pq.push({dist[adjNode], adjNode});
+                }
             }
         }
-    }
-    int sum =0;
-    for(int i=1;i<n+1;i++){
-        if(time[i] == 1e9)return -1;
-        else{
-            sum = max(sum, time[i]);
+
+        int ans = 0;
+        for(int i=1;i<n+1;i++){
+            // cout<<dist[i]<<" ";
+            if(dist[i] == 1e9)return -1;
+            ans = max(ans, dist[i]);
         }
+        return ans;
     }
-    return sum;
-}
 };
