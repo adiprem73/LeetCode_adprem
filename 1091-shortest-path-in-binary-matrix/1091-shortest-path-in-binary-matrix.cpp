06c138be8bbea41169b3caab607033eda2d55bc3
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-05 16:34:24 +0530

    Time: 14 ms (75.06%), Space: 26.2 MB (43.84%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-    int n = grid.size();
-    if(grid[0][0] == 1 || grid[n-1][n-1] == 1)return -1;
-    // make a visited matrix
-    vector<vector<int>> vis(n, vector<int>(n, 0));
-    // make a queue that can store (row, col, steps)
-    queue<pair<pair<int, int>, int>> q; // (row, col, steps)
-    // we always start with the starting node (0,0)
-    q.push({{0,0},1});
-    vis[0][0] = 1;
-
-    while(!q.empty()){
-        auto [coordinates, dist]= q.front();
-        q.pop();
-        int r = coordinates.first;
-        int c = coordinates.second;
-        if(r==n-1 && c == n-1)return dist;
-        for(int i=-1;i<=1;i++){
-            for(int j=-1;j<=1;j++){
-                int nr =r+i;
-                int nc =c+j;
-                if(nr>=0 && nr<n && nc>=0 && nc<n && vis[nr][nc] == 0 && grid[nr][nc] != 1){
-                    vis[nr][nc]= 1;
-                    q.push({{nr, nc}, dist + 1});
+        // build adjacency vector from adj amtrix
+        if(grid[0][0] == 1)return -1;
+        int n = grid.size();
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(n, vector<int>(n,-1));
+        q.push({{0,0},1});
+        vis[0][0]=1;
+        while(!q.empty()){
+            auto [cell, steps]=q.front();
+            q.pop();
+            int r = cell.first;
+            int c = cell.second;
+            if(r==n-1 && c==n-1)return steps;
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int nr = r+i;
+                    int nc = c+j;
+                    if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc] == 0 && vis[nr][nc] == -1){
+                        vis[nr][nc]=1;
+                        q.push({{nr,nc}, steps+1});
+                    }
                 }
             }
-        }
+        }        
+        return -1;
     }
-    return -1;
-}
 };
