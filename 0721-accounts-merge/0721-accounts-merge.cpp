435950070b857111468330ac8ceea735ec92f7fa
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-07 19:03:57 +0530

    Time: 28 ms (71.33%), Space: 40.3 MB (66.52%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,110 +1,77 @@
 class Solution {
-public:
-    class DisjointSet
-{
 
-public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++)
-        {
-            parent[i] = i;
+class DisjointSet{
+    private:
+    vector<int> parent, size;
+
+    public:
+    DisjointSet(int n){
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
+        size.resize(n+1, 1);
     }
 
-    int findUPar(int node)
-    {
-        if (node == parent[node])
-        {
-            return node;
-        }
-        return parent[node] = findUPar(parent[node]); // this is how path compression would take place. we store the parent in parent[node]
-    }
-    // Union by rank
-    void unionByRank(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
+    int findUParent(int u){
+        if(parent[u] == u)return u;
 
-        if (rank[ulp_u] < rank[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u])
-        {
-            parent[ulp_v] = ulp_u;
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
+        return parent[u] = findUParent(parent[u]);
     }
 
-    // Union by Size
-    void unionBySize(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
 
-        if (size[ulp_u] < size[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
+    void unionBySize(int u, int v){
+        int ulP_u = findUParent(u);
+        int ulP_v = findUParent(v);
+        if(ulP_u == ulP_v) return;
+
+        if(size[ulP_u] < size[ulP_v]){
+            parent[ulP_u]= ulP_v;
+            size[ulP_v]+= size[ulP_u];
         }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        else{
+            parent[ulP_v]= ulP_u;
+            size[ulP_u]+= size[ulP_v];
         }
     }
 };
 
-vector<vector<string>> accountsMerge(vector<vector<string>> &accounts)
-{
-    // 1st: iterate through all the array elements and account fo rhte overlapping of the email
-    unordered_map<string,int> mapMailNode;
-    int n = accounts.size();
-    DisjointSet ds(n);
-    for(int i=0;i<n;i++){
-        for(int j=1;j<accounts[i].size();j++){
-            string mail = accounts[i][j];
-            if(mapMailNode.find(mail) == mapMailNode.end()){
-                mapMailNode[mail] = i;
-            }else{
-                ds.unionBySize(i, mapMailNode[mail]);
+
+public:
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+        int n = accounts.size();
+        DisjointSet ds(n+1);
+        unordered_map<string,int> mp;
+        for(int i=0;i<n;i++){
+            int size = accounts[i].size();
+            for(int j=1;j<size;j++){
+                string mail = accounts[i][j];
+                if(mp.find(mail) == mp.end()){
+                    mp[mail]= i;
+                } 
+                else{
+                    ds.unionBySize(i, mp[mail]);
+                }               
             }
         }
-    }
-
-    vector<string> mergedMail[n];
-    for(auto it: mapMailNode){
-        string mail = it.first;
-        int node = ds.findUPar(it.second);
-        mergedMail[node].push_back(mail);
-    }
-
-    vector<vector<string>> ans;
-
-    for(int i=0;i<n;i++){
-        if(mergedMail[i].size() == 0)continue;
-
-        sort(mergedMail[i].begin(), mergedMail[i].end());
-        vector<string> temp;
-        temp.push_back(accounts[i][0]);
-        for(auto it: mergedMail[i]){
-            temp.push_back(it);
+        vector<string> mergedMail[n];
+        for(auto it: mp){
+            string mail = it.first;
+            int node = ds.findUParent(it.second);
+            mergedMail[node].push_back(mail);
         }
-        ans.push_back(temp);
+        vector<vector<string>> ans;
+        for(int i=0;i<n;i++){
+            if(mergedMail[i].size() == 0)continue;
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(accounts[i][0]);
+            for(auto it: mergedMail[i]){
+                temp.push_back(it);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
     }
-    return ans;
-}
 };
