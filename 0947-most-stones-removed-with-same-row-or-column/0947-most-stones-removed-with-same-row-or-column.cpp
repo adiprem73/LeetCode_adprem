14ec9ede35d7b68e1d263bde1517aa07e2886ed2
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-07 17:00:21 +0530

    Time: 15 ms (71.78%), Space: 25.6 MB (11.99%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,100 +1,70 @@
-class DisjointSet
-{
-
-public:
-    vector<int> rank, parent, size;
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 1);
-        parent.resize(n + 1);
-        for (int i = 0; i <= n; i++)
-        {
-            parent[i] = i;
-        }
-    }
-
-    int findUPar(int node)
-    {
-        if (node == parent[node])
-        {
-            return node;
-        }
-        return parent[node] = findUPar(parent[node]); // this is how path compression would take place. we store the parent in parent[node]
-    }
-    // Union by rank
-    void unionByRank(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-
-        if (rank[ulp_u] < rank[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u])
-        {
-            parent[ulp_v] = ulp_u;
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    // Union by Size
-    void unionBySize(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v)
-            return;
-
-        if (size[ulp_u] < size[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
-
-
 class Solution {
 public:
-    int removeStones(vector<vector<int>> &stones)
-{
-    int n = stones.size();
-    int numRows=0, numCols=0;
-    for(auto it: stones){
-        numRows = max(numRows, it[0]);
-        numCols = max(numCols, it[1]);
-    }
 
-    DisjointSet ds(numRows+numCols+1);
-    // all the nodes will not be inovlved with stones as not all stones are garuenteec to be rpesent at each row and olcumsn. so to trakc the empty rows and ocls we need to store the unique nodes
-    unordered_map<int,int> stoneNodes;
-    for(auto it: stones){
-        int nodeRow = it[0];
-        int nodeCol = it[1]+numRows+1;
-        ds.unionBySize(nodeRow, nodeCol);
-        stoneNodes[nodeRow]=1;
-        stoneNodes[nodeCol]=1;
-    }
-
-    int cnt =0;
-    for(auto it: stoneNodes){
-        if(ds.findUPar(it.first) == it.first){
-            cnt++; // this will countt eh number of connected components
+    // this question can be solved by seeing the coordinates as nodes of a graph. and we will have to form edges between the co-oridinates which share same row or same column
+    class DisjointSet{
+        private: 
+        vector<int> parent, size;
+        public:
+        DisjointSet(int n){
+            parent.resize(n+1);
+            for(int i=0;i<n+1;i++){
+                parent[i] = i;
+            }
+            size.resize(n+1, 1);
         }
+
+        int findUParent(int u){
+            if(parent[u] == u)return u;
+
+            return parent[u] = findUParent(parent[u]);
+        }
+
+        void unionBySize(int u, int v){
+            int ulP_u = findUParent(u);
+            int ulP_v = findUParent(v);
+
+            if(ulP_u == ulP_v)return; // already connected to the same components
+
+            if(size[ulP_v]>size[ulP_u]){
+                parent[ulP_u]= ulP_v;
+                size[ulP_v] += size[ulP_u];
+            }
+
+            else{
+                parent[ulP_v]= ulP_u;
+                size[ulP_u] += size[ulP_v];
+            }
+        }
+    };
+
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<vector<int>> adj(n);
+        int maxRow =0;
+        int maxCol =0;
+
+        for(auto it: stones){
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+
+        DisjointSet ds(maxRow + maxCol + 1);
+        unordered_map<int,int> stoneNodes;
+        for(auto it: stones){
+            int u = it[0];
+            int v = it[1] + maxRow + 1;
+            ds.unionBySize(u,v);
+            stoneNodes[u] = 1;
+            stoneNodes[v]= 1;
+        }
+        int cnt =0;
+        for(auto it: stoneNodes){
+            if(ds.findUParent(it.first) == it.first){
+                cnt++;
+            }
+        }
+
+        return n - cnt;
     }
-    return n-cnt;
-}
 };
