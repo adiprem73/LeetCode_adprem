3d60b8e35ca20d6cc3054388798dea4d0b6f8d5f
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-08 15:38:08 +0530

    Time: 11 ms (20.54%), Space: 20.8 MB (9.31%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-
-    bool dfs(int node, vector<int> &pathVis, vector<int> &vis, int n,  vector<vector<int>>& adj){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(auto adjNode : adj[node]){
-            if(vis[adjNode] == 0){ // it is already visited
-                if(dfs(adjNode, pathVis, vis, n, adj) == false){
-                    return false;
-                }
-            }else if(pathVis[adjNode] == 1){
-                return false;
-            }         
-        }
-        pathVis[node]=0;
-        return true;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-
+        // first build the adj vector
         vector<vector<int>> adj(numCourses);
         for(auto it: prerequisites){
             adj[it[0]].push_back(it[1]);
         }
 
-        // for this we need to detect cycles in an directed grpahs
-        // there are two ways to do this: 
-        // 1. using dfs(vis and pathVis)
-        // 2. using Kahn's Algorithm
-
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
+        vector<int> indegree(numCourses,0);
+        // update the indegrees
+        for(auto it: adj){
+            for(auto itr: it){
+                indegree[itr]++;
+            }
+        }
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(vis[i] == 0){
-                if(dfs(i, pathVis, vis, numCourses, adj) == false){
-                    return false;
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto adjNode: adj[node]){
+                indegree[adjNode] -- ;
+                if(indegree[adjNode] == 0){
+                    q.push(adjNode);
                 }
             }
         }
-        return true;
-
+        return topo.size() == numCourses;
     }
 };
