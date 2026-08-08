1fd9b596598578ac04a4defea98278d2a7dffae1
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-08 17:28:29 +0530

    Time: 760 ms (5.05%), Space: 44.2 MB (7.44%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,45 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        // build adjacency vector from adj amtrix
-        if(grid[0][0] == 1)return -1;
         int n = grid.size();
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(n, vector<int>(n,-1));
-        q.push({{0,0},1});
-        vis[0][0]=1;
-        while(!q.empty()){
-            auto [cell, steps]=q.front();
-            q.pop();
-            int r = cell.first;
-            int c = cell.second;
-            if(r==n-1 && c==n-1)return steps;
-            for(int i=-1;i<=1;i++){
-                for(int j=-1;j<=1;j++){
-                    int nr = r+i;
-                    int nc = c+j;
-                    if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc] == 0 && vis[nr][nc] == -1){
-                        vis[nr][nc]=1;
-                        q.push({{nr,nc}, steps+1});
+        if(grid[0][0]==1)return -1;
+        if (grid[n - 1][n - 1] == 1)
+            return -1;
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        queue<vector<int>> q;
+        q.push({0, 0}); //(row, col, steps)
+        vis[0][0] = 1;
+        
+        int steps = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            steps++;
+            for (int k = 0; k < size; k++) {
+                auto it = q.front();
+                q.pop();
+                int r = it[0];
+                int c = it[1];
+                // cout<<"Hello"<<endl;
+                if (r == n - 1 && c == n - 1)
+                    return steps;
+                // cout<<"Hello5"<<endl;
+                for (int i = -1; i <= 1; i++) {
+                    // cout<<"Hello6"<<endl;
+                    for (int j = -1; j <= 1; j++) {
+                        if(i==0 && j==0)continue;
+                        int nr = r + i;
+                        int nc = c + j;
+
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                            grid[nr][nc] == 0 && vis[nr][nc]==0) {
+                            cout << nr << " " << nc << endl;
+                            q.push({nr, nc});
+                            vis[nr][nc]=1;
+                        }
                     }
                 }
             }
-        }        
+        }
         return -1;
     }
 };
