61b87bd74ee472f19d16f01c16224c3f36806fae
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-08 14:37:22 +0530

    Time: 24 ms (34.94%), Space: 37.5 MB (48.21%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        // i will just use bfs
-        queue<pair<int,int>> q; //(row, col)
+        queue<pair<int,int>> q;
         int m = mat.size();
         int n = mat[0].size();
-        vector<vector<int>>vis(m, vector<int>(n, 0));
         for(int i=0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(mat[i][j]==0){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] == 0 ){
                     q.push({i,j});
-                    vis[i][j]=1;
                 }
             }
         }
-        int dr[]= {-1,0,1,0};
-        int dc[]= {0,1,0,-1};
         int steps=0;
-        
-
+        int dr[] = {-1,0,1,0};
+        int dc[] = {0,1,0,-1};
+        vector<vector<int>> vis(m, vector<int>(n,0));
         while(!q.empty()){
             int size = q.size();
             steps++;
             for(int i=0;i<size;i++){
-                auto [r,c] = q.front();
+                auto [r,c]= q.front();
                 q.pop();
-                for(int j=0;j<4;j++){
-                    int nr = r+dr[j];
-                    int nc = c+dc[j];
-                    // checking
-                    if(nr>=0 && nr<m && nc>=0 && nc<n){
-                        if(mat[nr][nc] != 0 && vis[nr][nc] == 0){
-                            vis[nr][nc]=1;
-                            mat[nr][nc]= steps;
-                            q.push({nr,nc});
-                        }
+                for(int i=0;i<4;i++){
+                    int nr = r+dr[i];
+                    int nc = c+dc[i];
+                    if(nr>= 0  && nr<m && nc>=0 && nc<n && mat[nr][nc]!=0 && vis[nr][nc]==0){
+                        mat[nr][nc]= steps;
+                        vis[nr][nc]= 1;
+                        q.push({nr,nc});
                     }
                 }
             }
