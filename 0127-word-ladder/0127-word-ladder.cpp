2446cde3da2948e950c0fd5b332e59de35b7264a
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-08 14:51:50 +0530

    Time: 61 ms (48.55%), Space: 21.2 MB (56.87%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-    
-
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
-        vector<int> used(wordList.size());
-        // we wil need to use BFS for this problem
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        // we will use a bfs approach since it will automatically give the shortest number of steps
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        // (steps, word),
         queue<pair<int, string>> q;
         q.push({1, beginWord});
-        unordered_set<string>st(wordList.begin(), wordList.end());
-        while (!q.empty()) {
+        while(!q.empty()){
             auto [steps, word] = q.front();
             q.pop();
-
-            // base case
-            if(word == endWord){
-                return steps;
-            }
-
-            for (int i = 0; i < word.size(); i++) {
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-                    char temp = word[i];
+            if(word == endWord) return steps;
+            for(int i=0;i<word.length();i++){
+                char temp = word[i];
+                for(char ch = 'a'; ch <= 'z';ch++){
                     word[i] = ch;
-                    if(st.find(word) != st.end()){
-                        q.push({steps+1, word});
+                    if(st.count(word) > 0){
                         st.erase(word);
+                        q.push({steps+1, word});
                     }
-                    word[i] = temp;
                 }
+                word[i] = temp;
             }
         }
-
         return 0;
     }
 };
