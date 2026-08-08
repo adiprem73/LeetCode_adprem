a3e34c170393aa9ae9c5d2c6adc6e762519df426
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-08 18:20:04 +0530

    Time: 15 ms (11.07%), Space: 18.7 MB (22.75%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,38 +1,43 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // (stops, node, cost)
+        // building the adjacency list
         vector<vector<pair<int,int>>> adj(n);
         for(auto it: flights){
-            adj[it[0]].push_back({it[1], it[2]});
+            int from = it[0];
+            int to = it[1];
+            int price = it[2];
+            adj[from].push_back({to, price});
         }
+        int cst =1e9;
+        priority_queue<vector<int>, vector<vector<int>>, greater<>>pq;
+        pq.push({0, 0, src});
+        vector<int> final(n, 1e9);
+        while(!pq.empty()){
+            auto it= pq.top();
+            pq.pop();
+            int stops = it[0];
+            int cost = it[1];
+            int node = it[2];
+            cout<<stops<<" "<<cost<<" "<<node<<endl;
 
-        queue<pair<int,pair<int,int>>> q;
-        q.push({0,{src,0}});
-        vector<int> dist(n, 1e9);
-        dist[src]=0;
+            // base cases
+            // if(stops>k){
+            //     return -1;
+            // }
+            if(node == dst && stops-1<=k)cst= min(cost, cst);
 
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-
-            int stops = it.first;
-            int node = it.second.first;
-            int cost = it.second.second;
-
-            if(stops>k)continue;
-            for(auto iter: adj[node]){
-                int adjNode = iter.first;
-                int edW = iter.second;
-
-                if(cost+edW < dist[adjNode] && stops <=k){
-                    dist[adjNode]= cost + edW;
-                    q.push({stops+1 , {adjNode, cost+edW}});
+            // relaxation for djikstra
+            for(auto adjNode: adj[node]){
+                int nextNode = adjNode.first;
+                int price = adjNode.second;
+                if(price+cost < final[nextNode]){
+                    final[nextNode] = price + cost;
+                    pq.push({stops+1, final[nextNode], nextNode});
                 }
             }
         }
-
-        if(dist[dst] == 1e9)return -1;
-
-        return dist[dst];
+        return cst == 1e9 ? -1:cst;
     }
 };
