f9d8878d90762af60ba1aaf2bceebfa9b0df4171
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-11 00:12:40 +0530

    Time: 6 ms (11.96%), Space: 12.8 MB (40.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
+        stack<int> st; //mono-tonic stack
         int n = nums2.size();
-        vector<int> nextGreater(n);
-        for(int i=n-1;i>=0;i--){
-            
-            while(!st.empty()){
-                if(st.top()<=nums2[i]){
-                    st.pop();
-                }else{
-                    break;
-                }
+        vector<int> nge(n);
+        st.push(nums2[n-1]);
+        nge[n-1]=-1;
+        unordered_map<int,int> mp;
+        mp[nums2[n-1]]= n-1;
+        for(int i=n-2;i>=0;i--){
+            mp[nums2[i]] = i;
+            while(!st.empty() && nums2[i] >= st.top()){
+                st.pop();
             }
-            if(!st.empty()){
-                nextGreater[i]= st.top();
-            }else{
-                nextGreater[i]= -1;
-            }
+            nge[i] = st.empty()? -1: st.top();
             st.push(nums2[i]);
         }
-        vector<int> ans;
+        vector<int> ans(nums1.size());
         for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<n;j++){
-                if(nums1[i] == nums2[j]){
-                    ans.push_back(nextGreater[j]);
-                }
-            }
+            ans[i] = nge[mp[nums1[i]]];
         }
         return ans;
     }
