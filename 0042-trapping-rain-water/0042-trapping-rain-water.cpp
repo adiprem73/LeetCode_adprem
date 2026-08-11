e476ec7f67be882f4d13d52c11178d4ea6b734b0
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-11 21:03:46 +0530

    Time: 0 ms (100%), Space: 26 MB (78.05%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // all we have to find is next gretaer and previous rgeater elelemtns
-        int max1=0;
         int n = height.size();
-        vector<int> left, right;
-        for(int i=0;i<n;i++){
-            left.push_back(max1);
-            max1 = max(max1, height[i]);
-        }
-        max1 =0;
-        for(int i=n-1;i>=0;i--){
-            right.push_back(max1);
-            max1 = max(max1, height[i]);
-        }
-        reverse(right.begin(), right.end());
+        int left = 0;
+        int right = n-1;
+        int leftMax = 0;
+        int rightMax =0;
         int ans =0;
-        // for(int i=0;i<n;i++){
-        //     cout<<left[i]<<" ";
-        // }
-        // cout<<endl;
-        // for(int i=0;i<n;i++){
-        //     cout<<right[i]<<" ";
-        // }
-        // cout<<endl;
-        for(int i=0;i<n;i++){
-            if( min(left[i], right[i])> height[i]){
-                ans += min(left[i], right[i])  - height[i];
+        while(left<= right){
+            if(height[left] <= height[right]){
+                // updating the left max
+                if(height[left] > leftMax){
+                    leftMax = height[left];
+                }else{
+                    ans += leftMax - height[left];
+                }
+                left++;
+            }else{
+                if(height[right] > rightMax){
+                    rightMax = height[right];
+                }else {
+                    ans+= rightMax - height[right];
+                }
+                right--;
             }
-            
         }
         return ans;
     }
