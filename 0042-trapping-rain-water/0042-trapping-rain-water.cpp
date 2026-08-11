33985f3c1ed824a6165f1879b64fdcd92e4cb250
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-11 19:35:23 +0530

    Time: 377 ms (4.83%), Space: 28.4 MB (6.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
-#define ll long long
-#define f(i, a, b) for(int i=a;i<b;i++)
-#define all(x) x.begin(),x.end()
-#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
-#define vmatprint(vv) for (auto& row : vv) { for (auto& elem : row) cout << elem << " "; cout << endl; } cout << endl;
-#define mprint(m) for (auto it : m) cout << it.first << " : " << it.second << endl; cout << endl;
-#define vint vector<int>
-#define vstring vector<string>
-#define vmat vector<vector<int>>
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define mii map<int,int>
-#define mll map<ll,ll>
-
-const int INF = 1e9;
-const ll MOD = 1e9+7;
     int trap(vector<int>& height) {
-        int n=height.size();
-    vint prefixmax(n);
-    vint suffixmax(n);
-    prefixmax[0]=height[0];
-    suffixmax[n-1]=height[n-1];
-    for(int i=1;i<n;i++){
-        prefixmax[i]=max(prefixmax[i-1],height[i]);
-    }
-    for(int i=n-2;i>=0;i--){
-        suffixmax[i]=max(suffixmax[i+1],height[i]);
-    }
-    // vprint(prefixmax);
-    // vprint(suffixmax);
-
-    int total=0;
-    for(int i=0;i<n;i++){
-        if(height[i]<prefixmax[i]&&height[i]<suffixmax[i]){
-            total+=min(prefixmax[i],suffixmax[i])-height[i];
+        // all we have to find is next gretaer and previous rgeater elelemtns
+        int max1=0;
+        int n = height.size();
+        vector<int> left, right;
+        for(int i=0;i<n;i++){
+            left.push_back(max1);
+            max1 = max(max1, height[i]);
         }
-    }
-    return total;
+        max1 =0;
+        for(int i=n-1;i>=0;i--){
+            right.push_back(max1);
+            max1 = max(max1, height[i]);
+        }
+        reverse(right.begin(), right.end());
+        int ans =0;
+        for(int i=0;i<n;i++){
+            cout<<left[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            cout<<right[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++){
+            if( min(left[i], right[i])> height[i]){
+                ans += min(left[i], right[i])  - height[i];
+            }
+            
+        }
+        return ans;
     }
 };
