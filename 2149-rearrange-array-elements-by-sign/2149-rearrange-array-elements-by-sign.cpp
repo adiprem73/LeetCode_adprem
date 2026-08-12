e995cd96946b756fd0aa890f224b4c0a15cdf622
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-12 13:07:21 +0530

    Time: 17 ms (5.34%), Space: 134.3 MB (27.2%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
-    #define ll long long
-#define push(a) push_back(a)
-#define f(i, a, b) for(int i=a;i<b;i++)
-#define all(x) x.begin(),x.end()
-#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
-#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
-#define vint vector<int>
-#define vstring vector<string>
-#define vmat vector<vector<int>>
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define mii map<int,int>
-#define mll map<ll,ll>
-
-const int INF = 1e9;
-const ll MOD = 1e9+7;
-
-vector<int> rearrangeArray(vector<int>& nums){
-    vint positive;
-    vint negative;
-    int n=nums.size();
-    for(int i=0;i<n;i++){
-        if(nums[i]<0){
-            negative.push_back(nums[i]);
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int len =nums.size();
+        int p=-1,n=-1;
+        for(int i=0;i<len;i++){
+            if(p==-1 && nums[i] >0){
+                p=i;
+            }
+            if(n==-1 && nums[i] <0){
+                n=i;
+            }
         }
-        else {
-            positive.push_back(nums[i]);
+        vector<int> ans;
+        while(p<len ||   n<len){
+            if(p<len){
+                ans.push_back(nums[p]);
+                p++;
+                while(p<len && nums[p]<0){
+                    p++;
+                }
+            }
+            if(n<len){
+                ans.push_back(nums[n]);
+                n++;
+                while(n<len && nums[n]>0){
+                    n++;
+                }
+            }
         }
+        return ans;
     }
-    int a=0,b=0;
-    vint ans;
-    for(int i=0;i<n;i++){
-        if(i%2==0){
-            ans.push_back(positive[a]);
-            a++;
-        }
-        else{
-            ans.push_back(negative[b]);
-            b++;
-        }
-    }
-    return ans;
-}
 };
