2749be44fdefc9d2b88476ae72f7d12b979a3426
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-12 12:43:16 +0530

    Time: 0 ms (100%), Space: 11.5 MB (98.28%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    #define ll long long
-#define push(a) push_back(a)
-#define f(i, a, b) for(int i=a;i<b;i++)
-#define all(x) x.begin(),x.end()
-#define vprint(v) for (auto& elem : v) cout << elem << " "; cout << endl;
-#define mprint(m) for (auto it:m) cout<<it.first<<" : "<<it.second<<endl; cout<<endl;
-#define vint vector<int>
-#define vstring vector<string>
-#define vmat vector<vector<int>>
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define mii map<int,int>
-#define mll map<ll,ll>
-
-const int INF = 1e9;
-const ll MOD = 1e9+7;
-
-void sortColors(vector<int>& nums) {
-    map<int,int> mp;
-    vint ans;
-    int n=nums.size();
-    for(int i=0;i<n;i++){
-        mp[nums[i]]++;
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int cnt0=0, cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+            if(num==0)cnt0++;
+            else if(num==1)cnt1++;
+            else cnt2++;
+        }
+        int i =0;
+        while(cnt0!=0){
+            nums[i]= 0;
+            i++;
+            cnt0--;
+        }
+        while(cnt1!=0){
+            nums[i]=1;
+            i++;
+            cnt1--;
+        }
+        while(cnt2!=0){
+            nums[i]=2;
+            i++;
+            cnt2--;
+        }
     }
-    for(int i=0;i<mp[0];i++){
-        ans.push_back(0);
-    }
-    for(int i=0;i<mp[1];i++){
-        ans.push_back(1);
-    }
-    for(int i=0;i<mp[2];i++){
-        ans.push_back(2);
-    }
-    nums=ans;
-}
 };
