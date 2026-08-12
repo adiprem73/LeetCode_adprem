0a72307782d430fdaf11ef919e9e651b87ae7a4e
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-12 16:11:30 +0530

    Time: 23 ms (57.94%), Space: 139.1 MB (72.46%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    deque <int> dq;
-    vector<int> ans;
-    for(int i=0;i<nums.size();i++){
-        // cout<<"for "<<i<<" : "<<endl;
-        // printdq(dq);
-
-        //kya window filled hai. agar filled hai to hi naa saamne wala element ko nikaalenge
-        if(!dq.empty() && dq.front()==i-k){
-            dq.pop_front();
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> dq;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int curr = nums[i];
+            if(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
+            }
+            while(!dq.empty() && nums[dq.back()]<=curr){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1)ans.push_back(nums[dq.front()]);
         }
-
-
-        //this makes sur ethat the front of a window always has windo's maximum.
-        while(!dq.empty() && nums[dq.back()] < nums[i]){
-            dq.pop_back();
-        }
-        
-        dq.push_back(i);
-
-        //we have completed the window
-        if(i>=k-1){
-
-            //this time this will only be the maximum. if this weren;t the max, it would have already been removed
-            ans.push_back(nums[dq.front()]);
-        }
-        
+        return ans;
     }
-    return ans;
-}
 };
