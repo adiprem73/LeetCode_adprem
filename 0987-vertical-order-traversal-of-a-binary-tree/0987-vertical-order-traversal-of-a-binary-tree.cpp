5f863102e3c7a5c5b8f3fc2d61774859661363f8
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-15 20:37:57 +0530

    Time: 0 ms (100%), Space: 15.9 MB (60%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,44 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
-    void travel(TreeNode* node, int r, int c,
-                map<int, map<int, multiset<int>>>& mp) {
-        if (node == nullptr)
-            return;
 
-        mp[c][r].insert(node->val);
-        if (node->left) {
-            travel(node->left, r + 1, c - 1, mp);
-        }
-        if (node->right) {
-            travel(node->right, r + 1, c + 1, mp);
-        }
+    void travel(TreeNode* node, int r, int c,
+                map<int, vector<pair<int,int>>>& mp) {
+
+        if (!node) return;
+
+        mp[c].push_back({r, node->val});
+
+        travel(node->left, r + 1, c - 1, mp);
+        travel(node->right, r + 1, c + 1, mp);
     }
 
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mp;
+
+        map<int, vector<pair<int,int>>> mp;
+
         travel(root, 0, 0, mp);
+
         vector<vector<int>> ans;
-        for (auto it : mp) {
+
+        for (auto &col : mp) {
+
+            auto &nodes = col.second;
+
+            sort(nodes.begin(), nodes.end());
+
             vector<int> curr;
-            for (auto itr : it.second) {
-                for (auto x : itr.second) {
-                    curr.push_back(x);
-                }
+
+            for (auto &[row, value] : nodes) {
+                curr.push_back(value);
             }
+
             ans.push_back(curr);
         }
+
         return ans;
     }
 };
