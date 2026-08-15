23d4cbce920b20d9253421aae25852d9535a343c
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-15 14:14:52 +0530

    Time: 0 ms (100%), Space: 15.1 MB (49.45%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,38 +12,30 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-    vector<vector<int>> result; //ds to store the final answer
-    if(root==NULL){ //if the tree is empty
-        return result;
-    }
+        // breadth first search
+        if(!root)return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> allLevels;
+        int x=0;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                auto node = q.front();
+                q.pop();
+                int val = node->val;
+                level.push_back(val);
 
-    queue<TreeNode*> q; //queue to store temporarily the nodes
-    q.push(root);
-    bool flag= true; //true means left to right insertion and false will do right to left
-
-    while(!q.empty()){
-        int size=q.size();
-        vector<int> lvl(size);
-
-        for(int i=0;i<size;i++){
-            TreeNode * node = q.front();
-            q.pop();
-
-            int index= flag ? i : (size-i-1); //this is where its decided whether to push from left to right or roght to left
-
-            lvl[index] = node->val;
-            if(node->left!=NULL){
-                q.push(node->left);
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
             }
-            if(node->right!=NULL){
-                q.push(node->right);
+            if(x%2!=0){
+                reverse(level.begin(), level.end());
             }
+            allLevels.push_back(level);
+            x++;
         }
-        flag= !flag;
-        result.push_back(lvl);
+        return allLevels;
     }
-
-    return result;
-
-}
 };
