3a7ae9edf994631981c783eb085b6c0797214da1
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-15 20:36:41 +0530

    Time: 7 ms (5.14%), Space: 17.3 MB (6.19%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,38 +6,36 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void travel(TreeNode* node, int r, int c, map<int, map<int, multiset<int>>> &mp){
-        if(node==nullptr)return;
+    void travel(TreeNode* node, int r, int c,
+                map<int, map<int, multiset<int>>>& mp) {
+        if (node == nullptr)
+            return;
 
         mp[c][r].insert(node->val);
-        if(node->left){
-            travel(node->left, r+1, c-1, mp);
+        if (node->left) {
+            travel(node->left, r + 1, c - 1, mp);
         }
-        if(node->right){
-            travel(node->right, r+1, c+1, mp);
+        if (node->right) {
+            travel(node->right, r + 1, c + 1, mp);
         }
     }
 
-
-
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         map<int, map<int, multiset<int>>> mp;
-        travel(root, 0, 0, mp);      
+        travel(root, 0, 0, mp);
         vector<vector<int>> ans;
-        for(auto it: mp){
+        for (auto it : mp) {
             vector<int> curr;
-            for(auto itr: it.second){
-                auto st = itr.second;
-                while(st.size() != 0){
-                    auto i = st.begin();
-                    curr.push_back(*i);
-                    st.erase(i);
-                }            
+            for (auto itr : it.second) {
+                for (auto x : itr.second) {
+                    curr.push_back(x);
+                }
             }
             ans.push_back(curr);
         }
