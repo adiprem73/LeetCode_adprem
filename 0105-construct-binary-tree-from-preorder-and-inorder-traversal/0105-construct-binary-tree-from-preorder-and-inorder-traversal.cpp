1fca31dd268d0bcca6b9532ca7dbe2ae31da66cd
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-16 12:11:30 +0530

    Time: 8 ms (22.36%), Space: 27.5 MB (54.06%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,32 +11,30 @@
  */
 class Solution {
 public:
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder)
-{
-    // first thing to do is to map the inorder using a simple ahshmap
-    map<int,int> inMap;
-    for(int i=0;i<inorder.size();i++){
-        inMap[inorder[i]]=i;
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int preStart, int preEnd, int inStart, int inEnd, map<int,int>& mp){
+        // base case
+        if(preStart>preEnd || inStart>inEnd){
+            return nullptr;
+        }
+
+        TreeNode* root = new TreeNode(preorder[preStart]); // will always b the root node
+
+        int inRoot = mp[preorder[preStart]];
+        int numsToLeft = inRoot - inStart;
+
+        root->left = build(preorder, inorder, preStart+1, preStart+numsToLeft, inStart, inRoot-1, mp);
+        root->right = build(preorder, inorder, preStart+numsToLeft+1, preEnd, inRoot+1, inEnd, mp);
+
+        return root;
     }
 
-    TreeNode *root = buildTree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap);
-    return root;
-}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        // hashing
+        map<int,int> mp;
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]] = i;
+        }
 
-TreeNode* buildTree(vector<int> & preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int,int> &inMap){
-    if(preStart> preEnd || inStart > inEnd) return NULL;
-
-    TreeNode* root = new TreeNode(preorder[preStart]);
-
-    int inroot = inMap[root->val];
-
-    int numsLeft = inroot- inStart;
-
-    root->left= buildTree(preorder, preStart+1, preStart+ numsLeft , inorder, inStart, inroot-1, inMap);
-
-    root->right= buildTree(preorder, preStart+ numsLeft+ 1, preEnd, inorder, inroot+1, inEnd, inMap);
-
-    return root;
-}
-
+        return build(preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1, mp);
+    }
 };
