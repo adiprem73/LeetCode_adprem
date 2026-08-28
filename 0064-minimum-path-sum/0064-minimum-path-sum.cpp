550b32cc1fe4eca48f44106dcb4c5c84ad33783c
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-29 02:05:47 +0530

    Time: 3 ms (30.21%), Space: 16.5 MB (16.31%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int func(int r, int c, vector<vector<int>> &grid, vector<vector<int>> &dp)
-{
+    int func(int r, int c, vector<vector<int>>& grid, vector<vector<int>> &dp){
+        int m = grid.size();
+        int n = grid[0].size();
+        // base case
+        if(r==m || c==n){
+            return 1e7;
+        }
+        if(r==m-1 && c==n-1)return grid[r][c];
 
-    // base cases
-    if(r<0 || c< 0){
-        return 1e9+7;
+        if(dp[r][c]!=-1)return dp[r][c];
+
+        int right=0, down =0;
+        // moving right
+        right = grid[r][c]+ func(r, c+1, grid, dp);
+
+        // moving down
+        down = grid[r][c]+ func(r+1, c, grid, dp);
+
+        return dp[r][c] = min(right, down);
     }
-    if(r==0 && c==0){
-        return grid[0][0];
+
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return func(0,0,grid, dp);
     }
-
-    // check for dp case
-    if(dp[r][c]!= -1){
-        return dp[r][c];
-    }
-
-    // move left
-    int left = func(r, c-1, grid, dp);
-    int right = func(r-1, c, grid, dp);
-
-    return dp[r][c] = grid[r][c] + min(left, right);
-}
-
-int minPathSum(vector<vector<int>> &grid)
-{
-    int m = grid.size();
-    int n = grid[0].size();
-
-    // make dp array
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    return func(m-1, n-1, grid, dp);
-}
 };
