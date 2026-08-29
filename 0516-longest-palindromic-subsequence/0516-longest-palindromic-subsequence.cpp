04d51f6768bb13fcb8312694212fe8b041c2674e
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-29 22:48:42 +0530

    Time: 95 ms (18.96%), Space: 75.9 MB (65.05%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int longestCommonSubsequenceSO(string text1, string text2) {
-    int n1=text1.length();
-    int n2=text2.length();
-    vector<int> prev(n2+1, 0);
-    for(int ind1=1;ind1<n1+1;ind1++){
-        vector<int> curr(n2+1, 0);
-        for(int ind2=1; ind2<n2+1;ind2++){
-            if(text1[ind1-1]==text2[ind2-1]){
-                curr[ind2]=1+ prev[ind2-1];
-            }else{
-                curr[ind2]= max(prev[ind2], curr[ind2-1]);
+    int lcs(int ind1, int ind2, string s1, string s2, vector<vector<int>>& dp) {
+        // base case
+        if (ind1 >= s1.size() || ind2 >= s2.size()) {
+            return 0;
+        }
+        // dp case
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+        int same = 0, diff = 0;
+        if (s1[ind1] == s2[ind2]) {
+            same = 1 + lcs(ind1 + 1, ind2 + 1, s1, s2, dp);
+        } else {
+            diff = max(lcs(ind1 + 1, ind2, s1, s2, dp),
+                       lcs(ind1, ind2 + 1, s1, s2, dp));
+        }
+        return dp[ind1][ind2] = same + diff;
+    }
+
+    int longestPalindromeSubseq(string s) {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        vector<vector<int>> dp(s.size() + 1, vector<int>(s.size() + 1, 0));
+
+        int n = s.size();
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= n; ind2++) {
+                int same = 0, diff = 0;
+                if (s[ind1-1] == s2[ind2-1]) {
+                    dp[ind1][ind2] =1 + dp[ind1-1][ind2-1];
+                } else {
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2],
+                               dp[ind1][ind2-1]);
+                }
             }
         }
-        prev=curr;
-    }
-    return prev[n2];
-}
 
-int longestPalindromeSubseq(string s) {
-    string s2=s;
-    reverse(s2.begin(),s2.end());
-    return longestCommonSubsequenceSO(s, s2);
-}
+        return dp[n][n];
+    }
 };
