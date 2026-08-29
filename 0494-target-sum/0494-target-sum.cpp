a2f918fbf051605bf4ca9c4469b32516f9cd7907
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-29 16:47:23 +0530

    Time: 955 ms (5.02%), Space: 11.8 MB (81.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int func(int ind, int target, vector<int>& nums){
-    int n = nums.size();
-    // base case
-    if(ind == n){
-        if(target == 0)return 1;
-        return 0;
+        // base case
+        int n = nums.size();
+        if(ind==n){
+            if(target ==0)return 1;
+            else return 0;
+        }
+
+        int add=0, sub=0;
+
+        add = func(ind+1, target- nums[ind], nums);
+        
+        sub = func(ind+1, target + nums[ind], nums);
+
+        return add+sub;
+        
     }
 
-
-    // additon 
-    int add = func(ind+1, target-nums[ind], nums);
-
-    // subtraction
-    int sub = func(ind+1, target+nums[ind], nums);
-
-    return add + sub;
-}
-
-int findTargetSumWays(vector<int> &nums, int target)
-{
-    int n = nums.size();
-    return func(0, target, nums);
-}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return func(0, target, nums);
+    }
 };
