fd1ac49e5e91ddd79ac846146bedca4b671c7510
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-29 19:34:53 +0530

    Time: 27 ms (58.06%), Space: 27.2 MB (79.49%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    int func(int ind1, int ind2, string &text1, string &text2, vector<vector<int>> &dp){
+    int func(int ind1, int ind2, string& text1, string& text2,
+             vector<vector<int>>& dp) {
         // base case
-        if(ind1 == text1.size() or ind2 == text2.size()){
+        if (ind1 == text1.size() or ind2 == text2.size()) {
             return 0;
         }
 
-        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
 
-        int same =0, diff=0;
+        int same = 0, diff = 0;
         // same
-        if(text1[ind1]==text2[ind2]){
-            same = 1+func(ind1+1, ind2+1, text1, text2, dp);
+        if (text1[ind1] == text2[ind2]) {
+            same = 1 + func(ind1 + 1, ind2 + 1, text1, text2, dp);
+        } else {
+            diff = max(func(ind1 + 1, ind2, text1, text2, dp),
+                       func(ind1, ind2 + 1, text1, text2, dp));
         }
-        else{
-            diff = max(func(ind1+1, ind2, text1, text2, dp),func(ind1, ind2+1, text1, text2, dp));
 
-        }
-
-        return dp[ind1][ind2]= same+diff;
+        return dp[ind1][ind2] = same + diff;
     }
 
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(text1.size(), vector<int>(text2.size(), -1));
-        return func(0, 0, text1, text2, dp);
+        vector<vector<int>> dp(text1.size() + 1,
+                               vector<int>(text2.size() + 1, 0));
+
+        for (int ind1 = text1.size()-1; ind1>=0; ind1--) {
+            for (int ind2 = text2.size()-1; ind2 >=0; ind2--) {
+                int same = 0, diff = 0;
+                // same
+                if (text1[ind1] == text2[ind2]) {
+                    same = 1 + dp[ind1+1][ind2+1];
+                } else {
+                    diff = max(dp[ind1+1][ind2], dp[ind1][ind2+1]);
+                }
+
+                dp[ind1][ind2] = same+diff;
+            }
+        }
+
+        return dp[0][0];
     }
 };
