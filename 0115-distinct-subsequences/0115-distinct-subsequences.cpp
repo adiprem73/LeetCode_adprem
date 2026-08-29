7d9447dc5069f1b5f07a331f8f93bb169ab40045
Aditya Prem <aditya.prem2003@gmail.com>
2026-08-30 00:05:55 +0530

    Time: 2776 ms (5.03%), Space: 422.2 MB (5.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    
-int numDistinct(string s, string t) {
-    int n=s.size();
-    int m=t.size();
-    vector<vector<long long>> dp(n+1, vector<long long> (m+1,0)); //now the indexing is all one based inside the dp table
-    //i think we might want to shift the dp table by one index
-
-    for(int i=0;i<=n;i++){ //i think this will solve the base case number 1 and base case number 2 isnt necessary since alreayd all the elements inside the dp table are initialised with 0
-        dp[i][0]=1;
-    }
-
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(s[i-1]==t[j-1]){
-                dp[i][j] = ( (__int128)dp[i-1][j-1] + dp[i-1][j] );
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
-            }
+    int func(int ps, int pt, string s, string t, vector<vector<int>>& dp){
+        // base case
+        if(pt<0){
+            return 1;
         }
+        if(ps<0){
+            return 0;
+        }
+
+        if(dp[ps][pt]!=-1)return dp[ps][pt];
+
+        int same=0, diff=0, notMatch=0;
+        if(s[ps] == t[pt]){
+            same = func(ps-1, pt-1, s, t, dp) + func(ps-1, pt, s, t, dp);
+        }
+        else{
+            diff = func(ps-1, pt, s, t, dp);
+        }
+        
+
+        return dp[ps][pt] = same + diff;
     }
-    return dp[n][m];
-}
+
+    int numDistinct(string s, string t) {
+        vector<vector<int>> dp(s.size()+1, vector<int>(t.size()+1, -1));
+
+        return func(s.size()-1,t.size()-1,s,t, dp);
+    }
 };
