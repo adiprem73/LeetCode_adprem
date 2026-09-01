9297685813b6bde9944f02c4ff0e156dd0e68e1c
Aditya Prem <aditya.prem2003@gmail.com>
2026-09-01 20:35:24 +0530

    Time: 3 ms (41.32%), Space: 12.9 MB (39.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st; //mono-tonic stack
+        // the very first task for me to do here would be to find the nge array fo nums2;
+        stack<int> st;
         int n = nums2.size();
         vector<int> nge(n);
-        st.push(nums2[n-1]);
-        nge[n-1]=-1;
-        unordered_map<int,int> mp;
-        mp[nums2[n-1]]= n-1;
-        for(int i=n-2;i>=0;i--){
-            mp[nums2[i]] = i;
-            while(!st.empty() && nums2[i] >= st.top()){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
-            nge[i] = st.empty()? -1: st.top();
+
+            if(st.empty()){
+                nge[i]  = -1;
+            }else{
+                nge[i] = st.top();
+            }
+
             st.push(nums2[i]);
         }
+
         vector<int> ans(nums1.size());
-        for(int i=0;i<nums1.size();i++){
-            ans[i] = nge[mp[nums1[i]]];
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[nums2[i]] = i;
         }
+
+        for(int i=0;i<nums1.size();i++){
+            if(mp.find(nums1[i]) != mp.end()){
+                ans[i] = nge[mp[nums1[i]]];
+            }else{
+                ans[i] = -1;
+            }
+        }
+
         return ans;
     }
 };
